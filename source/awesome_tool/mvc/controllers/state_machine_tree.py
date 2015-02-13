81cdbf15724f8d77591289c6300fdb49e428e101
Rico Belder <rico.belder@dlr.de>
2015-02-13 12:11:53 +0100

    finish functionalities of StateMachineTree - every single element is adjusted without influence to the other states - open TODO is that state selection result in tree selection too

--- source/awesome_tool/mvc/controllers/state_machine_tree.py
+++ source/awesome_tool/mvc/controllers/state_machine_tree.py
@@ -9,6 +9,7 @@ logger = log.get_logger(__name__)
 
 #TODO: comment
 
+
 class StateMachineTreeController(Controller):
 
     def __init__(self, model, view):
@@ -17,9 +18,10 @@ class StateMachineTreeController(Controller):
         """
         assert isinstance(model, StateMachineModel)
 
-        Controller.__init__(self, model, view, spurious=True)
-        #self.relieve_model(model)
-        self.observe_model(model.root_state)
+        Controller.__init__(self, model, view)
+        # self.relieve_model(model)
+        # self.observe_model(model.root_state)
+        # model.root_state.register_observer(self)
         self.tree_store = gtk.TreeStore(str, str, str, gobject.TYPE_PYOBJECT)
         view.set_model(self.tree_store)
         self.path_store = {}
@@ -31,44 +33,84 @@ class StateMachineTreeController(Controller):
     def register_adapters(self):
         pass
 
-    # TODO the update
     def update(self, changed_state_model=None):
-        # if changed_state_model:
-        #     print "PATH of container_changed: ", changed_state_model.state.get_path(), changed_state_model.state.state_id
-        #     #find_state
-        #     # find the changed state and modify tree
-        #     print "search: %s" % changed_state_model.state.name
-        #     for row in self.tree_store:
-        #         if row[3] is changed_state_model:
-        #             print "found changed container_state %s" % row
-        #             for child in row:
-        #                 print child
-        #             # for state_model in changed_state_model.states:
-        #             #     # check if missing
-        #             #     # check if in
-        # #else:
-        # self.path_store.clear()
-        self.tree_store.clear()
-        parent = self.tree_store.insert_before(None, None,
-                                               (self.model.root_state.state.name,
-                                                self.model.root_state.state.state_id,
-                                                self.model.root_state.state.state_type,
-                                                self.model.root_state))
-        # self.path_store[self.model.root_state.state.get_path()] = parent
-        for state_id, smodel in self.model.root_state.states.items():
-            self.insert_rec(parent, smodel)
-        # logger.debug("PATH_STORE: %s" % self.path_store)
+        """
+        Function checks if all states are in tree and if tree has states which were deleted
+        :param changed_state_model:
+        :return:
+        """
+        gtk.gdk.threads_enter()
 
-    def insert_rec(self, parent, state_model):
-        parent = self.tree_store.insert_before(parent, None,
-                                               (state_model.state.name,
-                                                state_model.state.state_id,
-                                                state_model.state.state_type,
-                                                state_model))
-        # self.path_store[state_model.state.get_path()] = parent
+        if changed_state_model:
+            if changed_state_model.parent is None:
+                parent_iter = self.path_store[changed_state_model.state.get_path()]
+            else:
+                parent_iter = self.path_store[changed_state_model.parent.state.get_path()]
+        else:
+            changed_state_model = self.model.root_state
+            parent_iter = None
+            self.path_store.clear()
+            self.tree_store.clear()
+
+        if not changed_state_model.state.get_path() in self.path_store:
+            own_root_state__model = ContainerStateModel(self.model.root_state.state)
+            parent_iter = self.tree_store.insert_before(parent_iter, None,
+                                                   (self.model.root_state.state.name,
+                                                    self.model.root_state.state.state_id,
+                                                    self.model.root_state.state.state_type,
+                                                    self.model.root_state))
+            #self.tree_store.row_inserted(path=self.tree_store.get_path(parent_iter), iter=parent_iter)
+            self.path_store[self.model.root_state.state.get_path()] = parent_iter
+        else:
+            parent_iter = self.path_store[self.model.root_state.state.get_path()]
+
+        # check if child are all in
+        for state_id, smodel in self.model.root_state.states.items():
+            self.insert_rec(parent_iter, smodel)
+
+        # check if child should not be in
+        for n in reversed(range(self.tree_store.iter_n_children(parent_iter))):
+            child_iter = self.tree_store.iter_nth_child(parent_iter, n)
+            path = self.tree_store.get_path(child_iter)
+            model = self.view.get_model()
+            if not model[path][1] in self.model.root_state.states:
+                self.tree_store.remove(child_iter)
+                #self.tree_store.row_deleted(self.tree_store.get_path(child_iter))
+
+        gtk.gdk.threads_leave()
+
+    def insert_rec(self, parent_iter, state_model):
+        # check if in
+        if not state_model.state.get_path() in self.path_store:
+            parent_iter = self.tree_store.insert_before(parent_iter, None,
+                                                   (state_model.state.name,
+                                                    state_model.state.state_id,
+                                                    state_model.state.state_type,
+                                                    state_model))
+            #self.tree_store.row_inserted(path=self.tree_store.get_path(parent_iter), iter=parent_iter)
+            self.path_store[state_model.state.get_path()] = parent_iter
+        else:
+            parent_iter = self.path_store[state_model.state.get_path()]
+            path = self.tree_store.get_path(parent_iter)
+            model = self.view.get_model()
+            if not state_model.state.state_type == model[path][2] or not state_model.state.name == model[path][0]:
+                model[path][0] = state_model.state.name
+                model[path][2] = state_model.state.state_type
+
+        # check if child are all in
         if type(state_model) is ContainerStateModel:
             for state_id, smodel in state_model.states.items():
-                self.insert_rec(parent, smodel)
+                self.insert_rec(parent_iter, smodel)
+
+        # check if child should not be in
+        for n in reversed(range(self.tree_store.iter_n_children(parent_iter))):
+            child_iter = self.tree_store.iter_nth_child(parent_iter, n)
+            path = self.tree_store.get_path(child_iter)
+            model = self.view.get_model()
+            # check if there are left over rows of old states (switch from HS or CS to S and so on)
+            if not type(state_model) is ContainerStateModel or not model[path][1] in state_model.states:
+                self.tree_store.remove(child_iter)
+                #self.tree_store.row_deleted(self.tree_store.get_path(child_iter))
 
     def on_cursor_changed(self, widget):
         (model, row) = self.view.get_selection().get_selected()
@@ -79,12 +121,19 @@ class StateMachineTreeController(Controller):
 
             self.model.selection.add(state_model)
 
-    @Controller.observe("states", after=True)
+
+    @Controller.observe("selection", after=True)
+    # # @Controller.observe("states", after=True)
+    # @Controller.observe("state_machine", after=True)
     def assign_notification_state(self, model, prop_name, info):
-        logger.debug("SM Tree State %s call_notification - AFTER:\n-%s\n-%s\n-%s\n-%s\n" %
-                    (self.model.root_state.state.state_id, prop_name, info.instance, info.method_name, info))
-        print "info: ", info
-        if hasattr(info.kwargs, 'model'):
-            self.update(info.kwargs.model)
-        else:
-            self.update()
+        # logger.debug("SM Tree State %s call_notification - AFTER:\n-%s\n-%s\n-%s\n-%s\n" %
+        #             (self.model.root_state.state.state_id, prop_name, info.instance, info.method_name, info))
+        # print "info: ", info
+        # if hasattr(info.kwargs, 'model'):
+        #     self.update(info.kwargs.model)
+        # else:
+        self.update(self.model.root_state)
+
+    # @Controller.observe("selection", after=True)
+    # def assign_notification(self, model, prop_name, info):
+    #     self.update(self.model.root_state)
