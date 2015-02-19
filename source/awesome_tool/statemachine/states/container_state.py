d6a897c112b8cdec4e4aa27a2f08e23d6f13ba0a
Franz Steinmetz <franz.steinmetz@dlr.de>
2015-02-19 08:47:50 +0100

    Delete transitions and data flows on state removal
    When removing a container state, its child states were removed. This was
    extended by also removing the remaining transitions and data flows (e.
    g. a data flow from a scoped variable to an output port)

--- source/awesome_tool/statemachine/states/container_state.py
+++ source/awesome_tool/statemachine/states/container_state.py
@@ -152,7 +152,7 @@ class ContainerState(State):
 
         """
         if not state_id in self.states:
-            raise AttributeError("State_id %s do not exist" % state_id)
+            raise AttributeError("State_id %s does not exist" % state_id)
 
         #first delete all transitions and data_flows in this state
         keys_to_delete = []
@@ -160,7 +160,6 @@ class ContainerState(State):
             if transition.from_state == state_id or transition.to_state == state_id:
                 keys_to_delete.append(key)
         for key in keys_to_delete:
-            # del self.transitions[key]
             self.remove_transition(key)
 
         keys_to_delete = []
@@ -168,14 +167,16 @@ class ContainerState(State):
             if data_flow.from_state == state_id or data_flow.to_state == state_id:
                 keys_to_delete.append(key)
         for key in keys_to_delete:
-            # del self.data_flows[key]
             self.remove_data_flow(key)
 
         # second delete all states in this state
         if isinstance(self.states[state_id], ContainerState):
             for child_state_id in self.states[state_id].states.keys():
-                # del self.states[state_id].states[child_state_id]
                 self.states[state_id].remove_state(child_state_id)
+            for transition_id in self.states[state_id].transitions.keys():
+                self.states[state_id].remove_transition(transition_id)
+            for data_flow_id in self.states[state_id].data_flows.keys():
+                self.states[state_id].remove_data_flow(data_flow_id)
 
         # final delete the state it self
         del self.states[state_id]
