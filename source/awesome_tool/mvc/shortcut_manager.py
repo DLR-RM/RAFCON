8095e8e57591cf0d2f2292dc91001d1fe11abfdb
Franz Steinmetz <franz.steinmetz@dlr.de>
2015-03-16 10:05:20 +0100

    Change key from <Escape> to Escape

--- source/awesome_tool/mvc/shortcut_manager.py
+++ source/awesome_tool/mvc/shortcut_manager.py
@@ -26,7 +26,7 @@ class ShortcutManager():
 
     def __init_shortcuts(self):
         self.__action_to_shortcuts = {
-            'abort': '<Escape>',
+            'abort': 'Escape',
             'copy': '<Control>C',
             'paste': '<Control>V',
             'cut': '<Control>X',
