332c4830fb827b36a2279b66c2d8897453636629
Franz Steinmetz <franz.steinmetz@dlr.de>
2015-03-16 10:05:53 +0100

    Listen to mouse move also without button pressed

--- source/awesome_tool/mvc/views/graphical_editor.py
+++ source/awesome_tool/mvc/views/graphical_editor.py
@@ -126,7 +126,7 @@ class GraphicalEditorView(View):
         # self.test_label = gtk.Label("Hallo")
         self.editor = GraphicalEditor(glconfig)
         self.editor.add_events(gtk.gdk.BUTTON_PRESS_MASK | gtk.gdk.BUTTON_RELEASE_MASK | gtk.gdk.BUTTON_MOTION_MASK |
-                               gtk.gdk.KEY_PRESS_MASK | gtk.gdk.KEY_RELEASE_MASK)
+                               gtk.gdk.KEY_PRESS_MASK | gtk.gdk.KEY_RELEASE_MASK | gtk.gdk.POINTER_MOTION_MASK)
         self.editor.set_size_request(500, 500)
         self.editor.set_flags(gtk.CAN_FOCUS)
 
