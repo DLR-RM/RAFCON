1117638d1cf163f77165230a9c69959c5a6d298e
Franz Steinmetz <franz.steinmetz@dlr.de>
2015-10-05 14:52:44 +0200

    Add new logging handler for LoggingView
    - Instead of abusing filters for logging messages to the LoggingView,
      a new class LoggingViewHandler is created
    - The handler is added to every new logger
    - A class member handles arbitrary many LoggingViews

--- source/rafcon/utils/log.py
+++ source/rafcon/utils/log.py
@@ -99,6 +99,7 @@ def format_log_record_for_view(record):
     if not sys.version_info < (2, 7):
         record.__setattr__("name", record.name.replace("rafcon.", ""))
     formatter = logging.Formatter("%(asctime)s: %(levelname)s - %(name)s:  %(message)s", "%H:%M:%S")
+    # full_format logging.Formatter("%(asctime)s: %(levelname)-8s - %(name)s:  %(message)s", "%H:%M:%S")
     return formatter.format(record)
 
 
@@ -106,37 +107,113 @@ debug_filter = DebugTextViewFilter()
 error_filter = ErrorTextViewFilter()
 
 
+class LoggingViewHandler(logging.Handler):
+
+    _logging_views = {}
+
+    def __init__(self):
+        super(LoggingViewHandler, self).__init__()
+
+        try:
+            unicode
+            self._unicode = True
+        except NameError:
+            self._unicode = False
+
+    @classmethod
+    def set_logging_view(cls, name, text_view):
+        cls._logging_views[name] = text_view
+
+    @classmethod
+    def remove_logging_view(cls, name):
+        if name in cls._logging_views:
+            del cls._logging_views[name]
+
+    def emit(self, record):
+        """Logs a new record
+
+        If a logging view is given, it is used to log the new record to. The code is partially copied from the
+        StreamHandler class
+        :param record:
+        :return:
+        """
+        try:
+            # Shorten the source name of the record (remove rafcon.)
+            if sys.version_info >= (2, 7):
+                record.__setattr__("name", record.name.replace("rafcon.", ""))
+            msg = self.format(record)
+            fs = "%s"
+            if not self._unicode:  # if no unicode support...
+                entry = fs % msg
+            else:
+                try:
+                    if isinstance(msg, unicode):
+                        ufs = u'%s'
+                        try:
+                            entry = ufs % msg
+                        except UnicodeEncodeError:
+                            entry = fs % msg
+                    else:
+                            entry = fs % msg
+                except UnicodeError:
+                    entry = fs % msg
+
+            for logging_view in self._logging_views.itervalues():
+                logging_view.print_message(entry, record.levelno)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
+
 # a dictionary to hold all loggers created so far
 existing_loggers = {}
 
+# global handler for logging on console and text views
+logging_view_handler = None
+
+
+def register_logging_view(name, logging_view):
+    LoggingViewHandler.set_logging_view(name, logging_view)
+
+
+def unregister_logging_view(name):
+    LoggingViewHandler.remove_logging_view(name)
+
+
 def get_logger(name):
-    """
+    """Generates and returns a logger object
+
     Returns a logger for a specific name i.e. a class name. There are several logging modes available:
     info, debug, warn, error
+
     :param name: the name of the new logger
-    :return:
     """
 
-    if name in existing_loggers.iterkeys():
+    if name in existing_loggers:
         return existing_loggers[name]
 
-    full = logging.Formatter("%(asctime)s: %(levelname)-8s - %(name)s:  %(message)s", "%H:%M:%S")
+    full_formatter = logging.Formatter("%(asctime)s: %(levelname)8s - %(name)s:  %(message)s", "%H:%M:%S")
 
-    stdout = logging.StreamHandler(sys.stdout)
-    stdout.setFormatter(full)
-    stdout.setLevel(logging.DEBUG)
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(full_formatter)
+    stdout_handler.setLevel(logging.DEBUG)
     no_higher_level_filter = NoHigherLevelFilter(logging.ERROR)
-    stdout.addFilter(no_higher_level_filter)
-    stdout.addFilter(debug_filter)
+    stdout_handler.addFilter(no_higher_level_filter)
+    # stdout_handler.addFilter(debug_filter)
 
-    stderr = logging.StreamHandler(sys.stderr)
-    stderr.setFormatter(full)
-    stderr.setLevel(logging.ERROR)
-    stderr.addFilter(error_filter)
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.setFormatter(full_formatter)
+    stderr_handler.setLevel(logging.ERROR)
+    # stderr_handler.addFilter(error_filter)
+
+    logging_view_handler = LoggingViewHandler()
+    logging_view_handler.setFormatter(full_formatter)
+    logging_view_handler.setLevel(logging.DEBUG)
 
     logger = logging.getLogger(name)
-    logger.addHandler(stdout)
-    logger.addHandler(stderr)
+    logger.addHandler(stdout_handler)
+    logger.addHandler(stderr_handler)
+    logger.addHandler(logging_view_handler)
 
     logger.setLevel(logging.DEBUG)
 
