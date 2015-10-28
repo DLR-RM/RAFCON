ffd9a36ec424cf889e0967aee593725055d31c47
Franz Steinmetz <franz.steinmetz@dlr.de>
2015-10-28 15:49:34 +0100

    New helper method to get view of core elements in canvas

--- source/rafcon/mvc/mygaphas/canvas.py
+++ source/rafcon/mvc/mygaphas/canvas.py
@@ -19,3 +19,19 @@ class MyCanvas(Canvas):
             if isinstance(item, (StateView, TransitionView, DataFlowView)) and item.model is model:
                 return item
         return None
+
+    def get_view_for_core_element(self, core_element, parent_item=None):
+        from rafcon.mvc.mygaphas.items.state import StateView
+        from rafcon.mvc.mygaphas.items.connection import DataFlowView, TransitionView
+        if parent_item is None:
+            items = self.get_all_items()
+        else:
+            items = self.get_children(parent_item)
+        for item in items:
+            if isinstance(item, StateView) and item.model.state is core_element:
+                return item
+            if isinstance(item, TransitionView) and item.model.transition is core_element:
+                return item
+            if isinstance(item, DataFlowView) and item.model.data_flow is core_element:
+                return item
+        return None
