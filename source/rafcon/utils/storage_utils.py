595359bbdcd840f03e255df28022c5de60719035
Franz Steinmetz <franz.steinmetz@dlr.de>
2015-11-02 14:34:32 +0100

    Refactor storage_utils
    - make method headers uniform
    - delete two duplicate methods
    - close files in all methods
    - fix docstrings

--- source/rafcon/utils/storage_utils.py
+++ source/rafcon/utils/storage_utils.py
@@ -14,39 +14,15 @@ import json
 import yaml
 
 
-def save_object_to_yaml_abs(object, abs_path):
-    """
-    Saves an object, which inherits from yaml.YAMLObject, to yaml file
-    :param object: the object to be saved
-    :param abs_path: the absolute path of the target yaml file
-    :return:
-    """
-    f = open(abs_path, 'w')
-    yaml.dump(object, f, indent=4)
-    f.close()
-
-
-def load_object_from_yaml(abs_path):
-    """
-    Loads an object, which inherits from yaml.YAMLObject, from a yaml file
-    :param object: the object to be saved
-    :param abs_path: the absolute path of the target yaml file
-    :return:
-    """
-    stream = file(abs_path, 'r')
-    state = yaml.load(stream)
-    return state
-
-
-def write_dict_to_yaml(dict_to_write, path, **kwords):
+def write_dict_to_yaml(dictionary, path, **kwargs):
     """
     Writes a dictionary to a yaml file
-    :param dict_to_write:  the dictionary to be written
+    :param dictionary:  the dictionary to be written
     :param path: the absolute path of the target yaml file
-    :return:
+    :param kwargs: optional additional parameters for dumper
     """
     f = open(path, 'w')
-    yaml.dump(dict_to_write, f, indent=4, **kwords)
+    yaml.dump(dictionary, f, indent=4, **kwargs)
     f.close()
 
 
@@ -56,37 +32,38 @@ def load_dict_from_yaml(path):
     :param path: the absolute path of the target yaml file
     :return:
     """
-    stream = file(path, 'r')
-    yaml_object = yaml.load(stream)
-    return yaml_object
+    f = file(path, 'r')
+    dictionary = yaml.load(f)
+    f.close()
+    return dictionary
 
 
-def write_dict_to_json(path, tmp_dict):
+def write_dict_to_json(dictionary, path, **kwargs):
     """
     Write a dictionary to a json file.
-    :param rel_path: The relative path to save the dictionary to
-    :param tmp_dict: The dictionary to get saved
-    :return:
+    :param path: The relative path to save the dictionary to
+    :param dictionary: The dictionary to get saved
+    :param kwargs: optional additional parameters for dumper
     """
     f = open(path, 'w')
-    json.dump(tmp_dict, f, indent=4)
+    json.dump(dictionary, f, indent=4)
     f.close()
 
 
 def load_dict_from_json(path):
-    """
-    Loads a dictionary from a json file.
-    :param rel_path: The relative path of the json file.
+    """Loads a dictionary from a json file.
+
+    :param path: The relative path of the json file.
     :return: The dictionary specified in the json file
     """
-    stream = file(path, 'r')
-    result = json.load(stream)
-    stream.close()
+    f = file(path, 'r')
+    result = json.load(f)
+    f.close()
     return result
 
 
 def create_path(path):
-    """ Creates a absolute path in the file system.
+    """Creates a absolute path in the file system.
 
     :param path: The path to be created
     :return:
@@ -96,7 +73,7 @@ def create_path(path):
 
 
 def remove_path(path):
-    """ emoves an absolute path in the file system
+    """Removes an absolute path in the file system
 
     :param path: The path to be removed
     :return:
