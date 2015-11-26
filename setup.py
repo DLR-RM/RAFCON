4300e5827c9f2794952bad01527e4b90540564d2
Franz Steinmetz <franz.steinmetz@dlr.de>
2015-11-26 15:16:49 +0100

    Add test command to setup.py
    Using python setup.py test, all test files are run.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 from os import path
+import sys
 
 
 def read_version_from_pt_file():
@@ -12,6 +14,32 @@ def read_version_from_pt_file():
                 version = parts[1].strip()
                 return version
     return 0
+
+
+class PyTest(TestCommand):
+    """Run py.test with RAFCON tests
+
+    Copied from https://pytest.org/latest/goodpractises.html#integrating-with-setuptools-python-setup-py-test
+    """
+    # This allows the user to add custom parameters to py.test, e.g.
+    # python setup.py test -a "-v"
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ['-x']
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        error_number = pytest.main(self.pytest_args)
+        sys.exit(error_number)
+
 
 setup(
     name='RAFCON',
@@ -34,6 +62,11 @@ setup(
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires=['docutils>=0.3']
+    # install_requires=['docutils>=0.3', 'numpy', 'OpenGL', 'twisted', 'gtkmvc', 'parser', 'gtk', 'astroid', 'json',
+    #                   'pyyaml'],
+
+    tests_require=['pytest'],
+
+    cmdclass={'test': PyTest},
 )
 
