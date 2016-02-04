735bf7c93dd07e0e46611c5c78fb88b5f46bb403
Annika Wollschlaeger <annika.wollschlaeger@dlr.de>
2016-02-04 12:41:32 +0100

    Add state icon functionality State is added if icon is clicked. Icon is highlighted on mouse over.

--- source/rafcon/mvc/controllers/state_icons.py
+++ source/rafcon/mvc/controllers/state_icons.py
@@ -28,6 +28,8 @@ class StateIconController(ExtendedController):
         self.view.connect("drag-data-get", self.on_drag_data_get)
         self.view.connect("drag-begin", self.on_drag_begin)
         self.view.connect("drag-end", self.on_drag_end)
+        self.view.connect("button-release-event", self.on_mouse_click)
+        self.view.connect("motion-notify-event", self.on_mouse_motion)
 
     def on_drag_data_get(self, widget, context, data, info, time):
         """dragged state is inserted and its state_id sent to the receiver
@@ -38,6 +40,32 @@ class StateIconController(ExtendedController):
         :param info:
         :param time:
         """
+        data.set_text(self._insert_state())
+
+    def on_drag_begin(self, widget, context):
+        """replace drag icon
+
+        :param widget:
+        :param context:
+        """
+        self.view.drag_source_set_icon_stock(gtk.STOCK_NEW)
+
+    def on_drag_end(self, widget, context):
+        self.view.unselect_all()
+
+    def on_mouse_click(self, widget, event):
+        if self.view.get_path_at_pos(int(event.x), int(event.y)) is not None \
+                and len(self.view.get_selected_items()) > 0:
+            self._insert_state()
+
+    def on_mouse_motion(self, widget, event):
+        path = self.view.get_path_at_pos(int(event.x), int(event.y))
+        if path is not None:
+            self.view.select_path(path)
+        else:
+            self.view.unselect_all()
+
+    def _insert_state(self):
         selected_path = self.view.states[self.view.get_selected_items()[0][0]]
 
         if selected_path is "ES":
@@ -50,15 +78,4 @@ class StateIconController(ExtendedController):
             state = BarrierConcurrencyState()
 
         if statemachine_helper.insert_state(state, False):
-            data.set_text(state.state_id)
-
-    def on_drag_begin(self, widget, context):
-        """replace drag icon
-
-        :param widget:
-        :param context:
-        """
-        self.view.drag_source_set_icon_stock(gtk.STOCK_NEW)
-
-    def on_drag_end(self, widget, context):
-        self.view.unselect_all()
+            return state.state_id
