800cd1223a803315ea4d1f8a0d90900b0796587c
Sebastian Brunner <sebastian.brunner@dlr.de>
2016-03-03 12:03:02 +0100

    fix absolute paths of library relocation unit test

--- source/test/common/test_library_relocation.py
+++ source/test/common/test_library_relocation.py
@@ -21,6 +21,7 @@ from rafcon.statemachine.states.barrier_concurrency_state import BarrierConcurre
 from rafcon.statemachine.execution.statemachine_execution_engine import StatemachineExecutionEngine
 from rafcon.statemachine.enums import StateExecutionState
 
+from os.path import join, dirname
 
 request_counter = 0
 
@@ -37,7 +38,7 @@ def open_folder(query):
     if request_counter == 0:
         return_value = None  # the first relocation has to be aborted
     else:
-        return_value = "/home_local/brun_sb/develop/rafcon/source/test_scripts/test_libraries/library1_for_relocation_test_relocated"
+        return_value = rafcon.__path__[0] + "/../test_scripts/test_libraries/library1_for_relocation_test_relocated"
 
     request_counter += 1
     return return_value
@@ -47,6 +48,14 @@ def test_library_relocation(caplog):
 
     signal.signal(signal.SIGINT, sm_singletons.signal_handler)
     testing_utils.test_multithrading_lock.acquire()
+
+    testing_utils.remove_all_libraries()
+
+    library_paths = rafcon.statemachine.config.global_config.get_config_value("LIBRARY_PATHS")
+    library_paths["test_scripts"] = rafcon.__path__[0] + "/../test_scripts"
+
+    # logger.debug(library_paths["test_scripts"])
+    # exit()
 
     interface.open_folder_func = open_folder
 
@@ -61,7 +70,7 @@ def test_library_relocation(caplog):
     sm_singletons.global_storage.base_path = RAFCON_TEMP_PATH_STORAGE
 
     [state_machine, version, creation_time] = rafcon.statemachine.singleton.global_storage.load_statemachine_from_path(
-        "/home_local/brun_sb/develop/rafcon/source/test_scripts/unit_test_state_machines/library_relocation_test")
+        rafcon.__path__[0] + "/../test_scripts/unit_test_state_machines/library_relocation_test")
 
     rafcon.statemachine.singleton.state_machine_manager.add_state_machine(state_machine)
 
@@ -72,6 +81,7 @@ def test_library_relocation(caplog):
     assert state_machine.root_state.output_data["output_0"] == 27
 
     testing_utils.assert_logger_warnings_and_errors(caplog, 0, 1)
+    testing_utils.reload_config(config=True, gui_config=False)
     testing_utils.test_multithrading_lock.release()
 
     logger.info("State machine execution finished!")
