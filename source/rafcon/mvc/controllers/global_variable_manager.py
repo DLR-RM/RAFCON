ed49e58afe39fe839946f9a9dc03471ca749918c
Rico Belder <rico.belder@dlr.de>
2016-03-08 09:29:01 +0100

    fix dimissing of gtk.entry widget for global-variables-list changes, according issue #87 -> still need fixing - introduce observation of the edited entry-widget (which has to debugged) -> commented - insert tab-movement controller (which alos need debugging) that is commented - introduce auto-selection and focus-check after delete and adding of new variables as in io-data-port-widget

--- source/rafcon/mvc/controllers/global_variable_manager.py
+++ source/rafcon/mvc/controllers/global_variable_manager.py
@@ -6,6 +6,7 @@ import gobject
 from gtk import ListStore
 import gtk
 
+from rafcon.mvc.controllers.utils import MoveAndEditWithTabKeyListFeatureController
 from rafcon.mvc.controllers.extended_controller import ExtendedController
 from rafcon.mvc.utils.comparison import compare_variables
 
@@ -23,9 +24,11 @@ class GlobalVariableManagerController(ExtendedController):
     def __init__(self, model, view):
         """Constructor"""
         ExtendedController.__init__(self, model, view)
+        # self.tab_edit_controller = MoveAndEditWithTabKeyListFeatureController(view['global_variable_tree_view'])
 
         self.global_variable_counter = 0
         self.global_variables_list_store = ListStore(gobject.TYPE_PYOBJECT)
+        self._actual_entry = None
 
     def register_view(self, view):
         """Called when the View was registered"""
@@ -51,33 +54,91 @@ class GlobalVariableManagerController(ExtendedController):
         view['locked_col'].set_cell_data_func(view['locked_text'], cell_text, self.model)
 
         view['name_text'].connect('edited', self.on_name_changed)
+        # view['name_text'].connect('editing-started', self.editing_started)
+        # view['name_text'].connect('editing-canceled', self.editing_canceled)
         view['value_text'].connect('edited', self.on_value_changed)
+        # view['value_text'].connect('editing-started', self.editing_started)
+        # view['value_text'].connect('editing-canceled', self.editing_canceled)
         view['new_global_variable_button'].connect('clicked', self.on_new_global_variable_button_clicked)
         view['delete_global_variable_button'].connect('clicked', self.on_delete_global_variable_button_clicked)
+
+        # self.tab_edit_controller.register_view()
 
     def register_adapters(self):
         """Adapters should be registered in this method call"""
         pass
 
-    def on_new_global_variable_button_clicked(self, widget, data=None):
+    def register_actions(self, shortcut_manager):
+        """Register callback methods for triggered actions
+
+        :param rafcon.mvc.shortcut_manager.ShortcutManager shortcut_manager: Shortcut Manager Object holding mappings
+            between shortcuts and actions.
+        """
+        shortcut_manager.add_callback_for_action("delete", self.on_delete_global_variable_button_clicked)
+        shortcut_manager.add_callback_for_action("add", self.on_new_global_variable_button_clicked)
+
+    def editing_started(self, renderer, editable, path):
+        """ Callback method to connect entry-widget focus-out-event to the respective change-method.
+        """
+        if self.view['name_text'] is renderer:
+            self._actual_entry = (editable, editable.connect('focus-out-event', self.change_name))
+        elif self.view['value_text'] is renderer:
+            self._actual_entry = (editable, editable.connect('focus-out-event', self.change_value))
+        else:
+            logger.error("Not registered Renderer was used")
+
+    def editing_canceled(self, event):
+        """ Callback method to disconnect entry-widget focus-out-event to the respective change-method.
+        """
+        if self._actual_entry is not None:
+            self._actual_entry[0].disconnect(self._actual_entry[1])
+            self._actual_entry = None
+
+    def change_name(self, entry, event):
+        """ Change-name-method to set the name of actual selected (row) global variable.
+        """
+        if self.view['global_variable_tree_view'].get_cursor()[0] is None or \
+                not self.view['global_variable_tree_view'].get_cursor()[0][0]:
+            return
+        self.on_name_changed(entry, self.view['global_variable_tree_view'].get_cursor()[0][0], text=entry.get_text())
+
+    def change_value(self, entry, event):
+        """ Change-value-method to set the value of actual selected (row) global variable.
+        """
+        if self.view['global_variable_tree_view'].get_cursor()[0] is None or \
+                not self.view['global_variable_tree_view'].get_cursor()[0][0]:
+            return
+        self.on_value_changed(entry, self.view['global_variable_tree_view'].get_cursor()[0][0], text=entry.get_text())
+
+    def on_new_global_variable_button_clicked(self, *args):
         """Triggered when the New button in the Global Variables tab is clicked
 
         Creates a new global variable with default values.
         """
-        new_global_variable = "new_global_%s" % self.global_variable_counter
-        self.global_variable_counter += 1
-        self.model.global_variable_manager.set_variable(new_global_variable, "value")
+        if self.view['global_variable_tree_view'].is_focus():
+            new_global_variable = "new_global_%s" % self.global_variable_counter
+            self.global_variable_counter += 1
+            self.model.global_variable_manager.set_variable(new_global_variable, "value")
+            for row_num, iter_elem in enumerate(self.global_variables_list_store):
+                if iter_elem[0][0] == new_global_variable:
+                    self.view['global_variable_tree_view'].set_cursor(row_num)
+                    break
 
-    def on_delete_global_variable_button_clicked(self, widget, data=None):
+    def on_delete_global_variable_button_clicked(self, *args):
         """Triggered when the Delete button in the Global Variables tab is clicked
 
         Deletes the selected global variable.
         """
-        tree_view = self.view["global_variable_tree_view"]
-        path = tree_view.get_cursor()[0]
-        if path is not None:
-            key = self.global_variables_list_store[int(path[0])][0][0]
-            self.model.global_variable_manager.delete_variable(key)
+        if self.view['global_variable_tree_view'].is_focus():
+            path = self.view["global_variable_tree_view"].get_cursor()[0]
+            if path is not None:
+                key = self.global_variables_list_store[int(path[0])][0][0]
+                try:
+                    self.model.global_variable_manager.delete_variable(key)
+                except AttributeError as e:
+                    logger.warning("Delete of globale variable '{0}' failed".format(key))
+            if len(self.global_variables_list_store) > 0:
+                self.view['global_variable_tree_view'].set_cursor(min(path, len(self.global_variables_list_store) - 1))
 
     def on_name_changed(self, widget, path, text):
         """Triggered when a global variable's name is edited
