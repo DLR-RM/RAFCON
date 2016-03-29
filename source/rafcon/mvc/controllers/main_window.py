4560bf4d69dbeacfe14cd05cd95c4148f1502696
Rico Belder <rico.belder@dlr.de>
2016-03-29 17:03:44 +0200

    fix slidely moving panes, set shrink=True for all undockable widgets and add unit-test - test is in docking-unit-test to check for interconnection - test will check if GUI only closed and opened changes it pane-positions -> that behavior should be avoid in future - adjust glade file to support improved reliable behavior and to avoid restrictions of the user by   shrink=False - adjust main-window-controller and - insert a TODO and some commented logger-messages to continue observation of some suspious internal   GUI signals which seem to disturbe performance

--- source/rafcon/mvc/controllers/main_window.py
+++ source/rafcon/mvc/controllers/main_window.py
@@ -179,6 +179,16 @@ class MainWindowController(ExtendedController):
         view.right_bar_window.initialize_title('STATE EDITOR')
         view.console_bar_window.initialize_title('CONSOLE')
 
+        # define panes
+        self.pane_id = {'LEFT_BAR_DOCKED_POS': 'top_level_h_pane',
+                        'RIGHT_BAR_DOCKED_POS': 'right_h_pane',
+                        'CONSOLE_DOCKED_POS': 'central_v_pane',
+                        'LEFT_BAR_INNER_PANE_POS': 'left_bar_pane'}
+        self.default_pane_pos = {'LEFT_BAR_DOCKED_POS': 300,
+                                 'RIGHT_BAR_DOCKED_POS': 1000,
+                                 'CONSOLE_DOCKED_POS': 600,
+                                 'LEFT_BAR_INNER_PANE_POS': 400}
+
     def register_view(self, view):
         self.register_actions(self.shortcut_manager)
 
@@ -248,10 +258,21 @@ class MainWindowController(ExtendedController):
         gui_helper.set_window_size_and_position(view.get_top_widget(), 'MAIN_WINDOW')
 
         # Initializing Pane positions
-        self.set_pane_position('RIGHT_BAR_DOCKED_POS', 'right_h_pane', default_pos=1000)
-        self.set_pane_position('LEFT_BAR_DOCKED_POS', 'top_level_h_pane', default_pos=300)
-        self.set_pane_position('CONSOLE_DOCKED_POS', 'central_v_pane', default_pos=600)
-        self.set_pane_position('LEFT_BAR_INNER_PANE_POS', 'left_bar_pane', default_pos=400)
+        # TODO check the child elements why those create so many size-allocate signals -> not clear why that happens
+        # for config_id, pane_id in self.pane_id.iteritems():
+        #     view[pane_id].get_child2().connect('size-allocate', self.print_paned_pos, config_id)
+        self.set_pane_position('LEFT_BAR_DOCKED_POS')
+        self.set_pane_position('RIGHT_BAR_DOCKED_POS')
+        self.set_pane_position('CONSOLE_DOCKED_POS')
+        self.set_pane_position('LEFT_BAR_INNER_PANE_POS')
+
+    def print_paned_pos(self, width, height, config_id):
+        pass
+        # if config_id in ['LEFT_BAR_DOCKED', 'RIGHT_BAR_DOCKED']:
+        pane_id = self.pane_id[config_id]
+        view = self.view[pane_id]
+        logger.warning("paned position {4} '{1}' is now {0}.{2}|{3}".format(view.get_position(), pane_id, width, height,
+                                                                            config_id))
 
     def connect_button_to_function(self, view_index, button_state, function):
         handler_id = self.view[view_index].connect(button_state, function)
@@ -270,16 +291,24 @@ class MainWindowController(ExtendedController):
         self.state_machine_execution_model = new_state_machine_execution_engine
         self.observe_model(self.state_machine_execution_model)
 
-    def set_pane_position(self, config_id, pane, default_pos=100):
+    def set_pane_position(self, config_id):
         """Adjusts the position of a GTK Pane to a value stored in the runtime config file. If there was no value
         stored, the pane's position is set to a default value.
 
         :param config_id: The pane identifier saved in the runtime config file
-        :param pane: The corresponding pane for which the position is to be adjusted
-        :param default_pos: A default value for the pane's position in case it was not stored in the runtime config
         """
+        default_pos = self.default_pane_pos[config_id]
         position = global_runtime_config.get_config_value(config_id, default_pos)
-        self.view[pane].set_position(position)
+        pane_id = self.pane_id[config_id]
+        # prev_pos = {}
+        # for pane_id_ in ['top_level_h_pane', 'right_h_pane', 'central_v_pane', 'left_bar_pane']:
+        #     prev_pos[pane_id_] = self.view[pane_id_].get_position()
+        self.view[pane_id].set_position(position)
+        # logger.info("try to set {0}, {1}, {2}, default: {3}, now on {4}"
+        #             "".format(pane_id, position, config_id, default_pos, self.view[pane_id].get_position()))
+        # for pane_id_ in ['top_level_h_pane', 'right_h_pane', 'central_v_pane', 'left_bar_pane']:
+        #     logger.info("after {0} position: {1} == {2}".format(pane_id_, self.view[pane_id_].get_position(),
+        #                                                         prev_pos[pane_id_]))
 
     def highlight_execution_of_current_sm(self, active):
         if self.get_controller('state_machines_editor_ctrl') is None or \
