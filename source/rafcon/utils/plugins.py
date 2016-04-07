a980fe1e1a9be0b894044350aea13b0ee5427a2f
Sebastian Brunner <sebastian.brunner@dlr.de>
2016-04-07 16:12:47 +0200

    add plugins module to enable plugin integration

---
+++ source/rafcon/utils/plugins.py
@@ -0,0 +1,60 @@
+"""
+.. module:: plugins
+   :platform: Unix, Windows
+   :synopsis: This is a module which can load all plugins specified in the RAFCON_PLUGIN_PATH environment variable and
+            can run all pre- and post init methods of all registered plugins
+
+.. moduleauthor:: Sebastian Brunner
+
+
+"""
+
+import sys
+import os
+import importlib
+
+from rafcon.utils import log
+logger = log.get_logger(__name__)
+
+
+plugin_dict = {}
+
+
+def load_plugins():
+    """
+    Loads all plugins specified in the RAFCON_PLUGIN_PATH environment variable
+    :return:
+    """
+    plugins = os.environ.get('RAFCON_PLUGIN_PATH', None)
+    plugin_list = plugins.split(":")
+    global plugin_dict
+    for plugin in plugin_list:
+        logger.info(plugin)
+        dir_name, plugin_name = os.path.split(plugin)
+        sys.path.append(dir_name)
+        i = importlib.import_module(plugin_name)
+        plugin_dict[plugin_name] = i
+
+
+def run_pre_inits():
+    """
+    Runs the pre_init methods of all registered plugins
+    :return:
+    """
+    global plugin_dict
+    for plugin, module in plugin_dict.iteritems():
+        # print dir(module)
+        # print module.__file__
+        module.hooks.pre_init()
+
+
+def run_post_inits(setup_config):
+    """
+    Runs the post_init methods of all registered plugins
+    :param setup_config:
+    :return:
+    """
+    global plugin_dict
+    for plugin, module in plugin_dict.iteritems():
+        module.hooks.post_init(setup_config)
+
