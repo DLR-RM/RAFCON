b3767a84d986e2264ccbc3b5dfb05e1e2478b9c8
Sebastian Brunner <sebastian.brunner@dlr.de>
2016-04-29 09:31:03 +0200

    fix last_data_wins_test
    fix small logic bug in LAIEIY/script.py
    applied modern art to overview converter to state machine layout :-)

--- source/test_scripts/unit_test_state_machines/last_data_wins_test/WBNVOM/LAIEIY/script.py
+++ source/test_scripts/unit_test_state_machines/last_data_wins_test/WBNVOM/LAIEIY/script.py
@@ -2,7 +2,7 @@
 def execute(self, inputs, outputs, gvm):
     if inputs['input_1'] == 0:
         self.logger.info("Last wins! -> value: {}".format(inputs['input_1']))
-    elif inputs['input_1'] == -1:
+    elif inputs['input_1'] == 5:
         self.logger.error("Not Last wins! -> value: {}".format(inputs['input_1']))
     else:
         self.logger.error("Something wins! -> value: {}".format(inputs['input_1']))
