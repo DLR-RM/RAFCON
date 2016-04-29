933216f5ada5ee41422ae425addb512806c4169e
Franz Steinmetz <franz.steinmetz@dlr.de>
2016-04-29 17:32:32 +0200

    Adapt multi_event_test to changed in core

--- source/test_scripts/unit_test_state_machines/multi_events_test/YLQIGU/GRPGBX/OGFHFG/script.py
+++ source/test_scripts/unit_test_state_machines/multi_events_test/YLQIGU/GRPGBX/OGFHFG/script.py
@@ -1,29 +1,30 @@
 def print_state_events(state):
     state.logger.debug("Value of preemted event: {0}".format(str(state.preempted)))
-    state.logger.debug("Value of running event: {0}".format(str(state.running)))
+    state.logger.debug("Value of started event: {0}".format(str(state.started)))
     state.logger.debug("Value of paused event: {0}".format(str(state.paused)))
 
 def execute(self, inputs, outputs, gvm):
     self.logger.debug("Hello world")
-    self.logger.debug("Value of execution_events: {0}".format(str(self.execution_events)))
+    self.logger.debug("Value of _interruption_events: {0}".format(str(self._interruption_events)))
+    self.logger.debug("Value of _unpause_events: {0}".format(str(self._unpause_events)))
     
     gvm.set_variable("sm_status", 0)
     
-    self.wait_for_execution_events()
+    self.wait_for_interruption()
     print_state_events(self)
     if self.paused:
         gvm.set_variable("paused", True)
     
     gvm.set_variable("sm_status", 1)
     
-    self.wait_for_execution_events()
+    self.wait_for_unpause()
     print_state_events(self)
-    if self.running:
+    if self.started:
         gvm.set_variable("running", True)
 
     gvm.set_variable("sm_status", 2)
 
-    self.wait_for_execution_events()
+    self.wait_for_interruption()
     print_state_events(self)
     if self.preempted:
         gvm.set_variable("preempted", True)
