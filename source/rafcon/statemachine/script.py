54838e77716ae9bdf741f4122eab13fb99a0152f
Franz Steinmetz <franz.steinmetz@dlr.de>
2016-05-03 10:08:07 +0200

    Do not add script modules to sys.modules
    - modules temporary generated for the execute script are no longer added to the sys.modules
    - a lock is acquired while generating the module
    - docstrings are fixed
    - change temporary name of unsaved scripts
    - remove commented code

--- source/rafcon/statemachine/script.py
+++ source/rafcon/statemachine/script.py
@@ -4,22 +4,20 @@
    :synopsis: A module to represent the script file for each state in a state machine
 
 .. moduleauthor:: Sebastian Brunner
-
-
 """
 
 import os
 import imp
-import sys
 import yaml
 from gtkmvc import Observable
 
+from rafcon.statemachine.enums import DEFAULT_SCRIPT_PATH
 from rafcon.statemachine.id_generator import *
 import rafcon.statemachine.singleton
+
 from rafcon.utils import filesystem
 from rafcon.utils import log
 logger = log.get_logger(__name__)
-from rafcon.statemachine.enums import DEFAULT_SCRIPT_PATH
 
 
 DEFAULT_SCRIPT_FILE = "default_script.py"
@@ -28,7 +26,6 @@ DEFAULT_SCRIPT = filesystem.read_file(os.path.dirname(__file__), DEFAULT_SCRIPT_
 
 
 class Script(Observable, yaml.YAMLObject):
-
     """A class for representing the script file for each state in a state machine
 
     It inherits from Observable to make a change of its fields observable.
@@ -59,17 +56,17 @@ class Script(Observable, yaml.YAMLObject):
             if not os.path.exists(self._path):
                 os.makedirs(self._path)
             if not filename:
-                self._filename = "Script_%s.file" % str(self._script_id)
+                self._filename = "tmp_script.file"
             filesystem.write_file(os.path.join(self._path, self._filename), self.script)
 
         if check_path:
             if not os.path.exists(self._path):
-                raise RuntimeError("Path %s does not exist" % self._path)
+                raise RuntimeError("Script path '{}' does not exist".format(self._path))
             if not os.path.exists(os.path.join(self._path, self._filename)):
-                raise RuntimeError("Path %s does not exist" % os.path.join(self._path, self._filename))
+                raise RuntimeError("Script '{}' does not exist".format(os.path.join(self._path, self._filename)))
 
             # load and build the module per default else the default scripts will be loaded in self.script
-            self.load_script()
+            self._load_script()
             self.build_module()
 
     def reload_path(self, filename=None):
@@ -78,12 +75,14 @@ class Script(Observable, yaml.YAMLObject):
             self._filename = filename
 
     def execute(self, state, inputs=None, outputs=None, backward_execution=False):
-        """
-        Execute the custom "execute" function specified in the script.
-        :param state: the state to which runs the execute function
-        :param inputs: the input data of the script
-        :param outputs: the output data of the script
-        :return:
+        """Execute the user 'execute' function specified in the script
+
+        :param ExecutionState state: the state belonging to the execute function, refers to 'self'
+        :param dict inputs: the input data of the script
+        :param dict outputs: the output data of the script
+        :param bool backward_execution: Flag whether to run the script in backwards mode
+        :return: Return value of the execute script
+        :rtype: str | int
         """
         if not outputs:
             outputs = {}
@@ -101,8 +100,8 @@ class Script(Observable, yaml.YAMLObject):
             return self._compiled_module.execute(state, inputs, outputs,
                                                  rafcon.statemachine.singleton.global_variable_manager)
 
-    def load_script(self):
-        """Loads and builds the module given by the path and the filename
+    def _load_script(self):
+        """Loads the script from the filesystem
         """
         script_text = filesystem.read_file(self._path, self._filename)
 
@@ -112,32 +111,26 @@ class Script(Observable, yaml.YAMLObject):
         self.script = script_text
 
     def build_module(self):
-        """Loads and builds the module given by the path and the filename
+        """Builds a temporary module from the script file
         """
-
-        module_name = os.path.splitext(self._filename)[0] + str(self._script_id)
-
-        # load module
-        tmp_module = imp.new_module(module_name)
-        sys.modules[module_name] = tmp_module
-
-        code = compile(self.script, '%s (%s)' % (self._filename, self._script_id), 'exec')
-
         try:
-            exec code in tmp_module.__dict__
-        except RuntimeError, e:
-            raise IOError("The compilation of the script module failed - error message: %s" % str(e))
+            imp.acquire_lock()
+            module_name = os.path.splitext(self._filename)[0] + str(self._script_id)
 
-        # return the module
-        self.compiled_module = tmp_module
+            # load module
+            tmp_module = imp.new_module(module_name)
 
-    # def get_path(self):
-    #     """
-    #     Returns the path of the script.
-    #     Note: Normally you should use the get_filesystem_path of the state class.
-    #     :return: the path where this script resides
-    #     """
-    #     return self._path
+            code = compile(self.script, '%s (%s)' % (self._filename, self._script_id), 'exec')
+
+            try:
+                exec code in tmp_module.__dict__
+            except RuntimeError, e:
+                raise IOError("The compilation of the script module failed - error message: %s" % str(e))
+
+            # return the module
+            self.compiled_module = tmp_module
+        finally:
+            imp.release_lock()
 
     @classmethod
     def to_yaml(cls, dumper, data):
@@ -164,8 +157,7 @@ class Script(Observable, yaml.YAMLObject):
 
     @property
     def compiled_module(self):
-        """Property for the _compiled_module field
-
+        """Return the compiled module
         """
         return self._compiled_module
 
