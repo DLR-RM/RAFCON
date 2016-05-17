ac383a0deb231966435a245bb6ed8e7b2b15e1fb
Sebastian Brunner <sebastian.brunner@dlr.de>
2016-05-17 14:19:07 +0200

    rewrite prints to logs
    add state layout for to be able to see the transitions

--- source/test_scripts/unit_test_state_machines/error_propagation_test/BNXYRR/ZHIMZX/script.py
+++ source/test_scripts/unit_test_state_machines/error_propagation_test/BNXYRR/ZHIMZX/script.py
@@ -1,10 +1,10 @@
 
 
 def execute(self, inputs, outputs, gvm):
-    print "Handling hierarchy abortion ... "
+    self.logger.info( "Handling hierarchy abortion ... ")
     
     if "error" in inputs:
-        print type(inputs["error"]).__name__, ":", inputs["error"]
+        self.logger.info(str(type(inputs["error"]).__name__) +  ": " + str(inputs["error"]))
         outputs["error_check"] = "successfull"
     
     return 0
