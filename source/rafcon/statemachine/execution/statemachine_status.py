5c7f7b3d6cc1a4ea06cf9f06aa0456024425cc48
Sebastian Brunner <sebastian.brunner@dlr.de>
2016-05-18 09:01:26 +0200

    clean state machine status class

--- source/rafcon/statemachine/execution/statemachine_status.py
+++ source/rafcon/statemachine/execution/statemachine_status.py
@@ -25,9 +25,6 @@ class StateMachineStatus(Observable):
 
     :ivar execution_mode: the execution mode of the state machine
                         (i.e. running, paused, stopped, stepping)
-    :ivar _dependency_tree: when starting the state machine from an arbitrary child state, a dependency tree is
-                            calculated and stored here
-    :ivar _thread_histories: set of threads that are currently executing inside the state machine
 
     """
 
@@ -41,45 +38,10 @@ class StateMachineStatus(Observable):
         self.execution_mode = execution_mode
         logger.debug("Statemachine status is set to %s" % str(execution_mode))
         self.execution_condition_variable = Condition()
-        self._dependency_tree = None
-        self._thread_histories = []
-
-    def set_thread_history(self, history_id, execution_history):
-        """ Sets the execution history for a certain history_id
-
-        :param history_id: the id of the history
-        :param execution_history: the execution history of a certain thread
-        :return:
-        """
-        if not isinstance(execution_history, ExecutionHistory):
-            raise TypeError("execution_history must be of type ExecutionHistory")
-        self._thread_histories[history_id] = execution_history
-
-    def set_execution_mode(self, execution_mode):
-        """ Sets the current execution mode
-
-        :param execution_mode: the new execution mode to set
-        :return:
-        """
-        self.execution_mode = execution_mode
 
     #########################################################################
     # Properties for all class fields that must be observed by gtkmvc
     #########################################################################
-
-    @property
-    def dependency_tree(self):
-        """Property for the _dependency_tree field
-
-        """
-        return self._dependency_tree
-
-    @property
-    def thread_histories(self):
-        """Property for the _thread_histories field
-
-        """
-        return self._thread_histories
 
     @property
     def execution_mode(self):
