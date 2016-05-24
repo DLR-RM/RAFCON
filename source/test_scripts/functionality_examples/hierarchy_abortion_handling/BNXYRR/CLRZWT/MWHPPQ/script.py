5f20891dce9c900e46c59615fa9d207ad1a885d3
Sebastian Brunner <sebastian.brunner@dlr.de>
2016-05-24 15:59:58 +0200

    example on abortion handling
    this is an example of how the error can be handled if the error handling state is a hierarchy state

---
+++ source/test_scripts/functionality_examples/hierarchy_abortion_handling/BNXYRR/CLRZWT/MWHPPQ/script.py
@@ -0,0 +1,13 @@
+
+
+def execute(self, inputs, outputs, gvm):
+    print "Handling execution abortion ... "
+    
+    if "error" in inputs:
+        print inputs["error"]
+        
+    number1 = 0
+    number2 = 1 / number1
+    
+    return 0
+
---
+++ source/test_scripts/functionality_examples/hierarchy_abortion_handling/BNXYRR/PAKLTC/ZHIMZX/script.py
@@ -0,0 +1,11 @@
+
+
+def execute(self, inputs, outputs, gvm):
+    self.logger.info( "Handling hierarchy abortion ... ")
+    
+    if "error" in inputs:
+        self.logger.info(str(type(inputs["error"]).__name__) +  ": " + str(inputs["error"]))
+        if type(inputs["error"]) is ZeroDivisionError:
+            outputs["error_check"] = "successfull"
+    
+    return 0
