a6bfbc6f641578028c4c5921deb480b9e0a6edf1
Sebastian Brunner <sebastian.brunner@dlr.de>
2016-05-24 07:48:20 +0200

    enhance error propagation test
    in the last abortion handling state the type of the error is checked before marking the test as successful

--- source/test_scripts/unit_test_state_machines/error_propagation_test/BNXYRR/ZHIMZX/script.py
+++ source/test_scripts/unit_test_state_machines/error_propagation_test/BNXYRR/ZHIMZX/script.py
@@ -5,6 +5,7 @@ def execute(self, inputs, outputs, gvm):
     
     if "error" in inputs:
         self.logger.info(str(type(inputs["error"]).__name__) +  ": " + str(inputs["error"]))
-        outputs["error_check"] = "successfull"
+        if type(inputs["error"]) is ZeroDivisionError:
+            outputs["error_check"] = "successfull"
     
     return 0
