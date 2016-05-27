cb9dfe920710554a57a07c1561fd84ac2d41c62d
Franz Steinmetz <franz.steinmetz@dlr.de>
2016-05-27 12:45:54 +0200

    Gaphas: Fix recursive removal of state

--- source/rafcon/mvc/mygaphas/items/state.py
+++ source/rafcon/mvc/mygaphas/items/state.py
@@ -117,11 +117,12 @@ class StateView(Element):
     def remove(self):
         """Remove recursively all children and then the StateView itself
         """
-        children = self.canvas.get_children(self)
+        children = self.canvas.get_children(self)[:]
         for child in children:
             if isinstance(child, StateView):
                 child.remove()
-            self.canvas.remove(child)
+            if isinstance(child, NameView):
+                self.canvas.remove(child)
         self.remove_keep_rect_within_constraint_from_parent()
         self.canvas.remove(self)
 
