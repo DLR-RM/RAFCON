977be20ea6bc8240bfd493d14b663e6633e53c64
Rico Belder <rico.belder@dlr.de>
2016-08-01 15:25:47 +0200

    start script test: is user save now - refactor naming to make clear that the file is used in respective state machine - assignment the file name only once

--- source/test/common/test_start_script.py
+++ source/test/common/test_start_script.py
@@ -5,7 +5,10 @@ import subprocess
 import os
 import sys
 import testing_utils
-# /tmp/rafcon/unittests/test_start_script.txt??? /tmp/rafcon_unit_test/test_start_script.txt
+FILE_MODIFIED_BY_STATE_MACHINE = os.path.join(testing_utils.RAFCON_TEMP_PATH_TEST_BASE_ONLY_USER_SAVE,
+                                              "test_start_script.txt")
+if os.path.exists(FILE_MODIFIED_BY_STATE_MACHINE):
+    os.remove(FILE_MODIFIED_BY_STATE_MACHINE)
 
 
 def get_rafcon_env():
@@ -21,11 +24,11 @@ def test_start_script_open():
     cmd = sys.executable + " %s -o %s" % (script, start_path)
     cmd_res = subprocess.call(cmd, shell=True, env=get_rafcon_env())
     assert cmd_res == 0
-    tmp_file = open("/tmp/rafcon_unit_tests/test_start_script.txt", "r")
+    tmp_file = open(FILE_MODIFIED_BY_STATE_MACHINE, "r")
     res = tmp_file.read()
     tmp_file.close()
     assert (res == "start, state, "), "start script failed"
-    os.remove("/tmp/rafcon_unit_tests/test_start_script.txt")
+    os.remove(FILE_MODIFIED_BY_STATE_MACHINE)
 
 
 def test_start_script_state():
@@ -36,11 +39,11 @@ def test_start_script_state():
     cmd = sys.executable + " %s -o %s -s %s" % (script, start_path, state_path)
     cmd_res = subprocess.call(cmd, shell=True, env=get_rafcon_env())
     assert cmd_res == 0
-    tmp_file = open("/tmp/rafcon_unit_tests/test_start_script.txt", "r")
+    tmp_file = open(FILE_MODIFIED_BY_STATE_MACHINE, "r")
     res = tmp_file.read()
     tmp_file.close()
     assert (res == "state, "), "start from state failed"
-    os.remove("/tmp/rafcon_unit_tests/test_start_script.txt")
+    os.remove(FILE_MODIFIED_BY_STATE_MACHINE)
 
 
 def test_start_script_valid_config():
@@ -51,11 +54,11 @@ def test_start_script_valid_config():
     cmd = sys.executable + " %s -o %s -c %s" % (script, start_path, config)
     cmd_res = subprocess.call(cmd, shell=True, env=get_rafcon_env())
     assert cmd_res == 0
-    tmp = open("/tmp/rafcon_unit_tests/test_start_script.txt", "r")
+    tmp = open(FILE_MODIFIED_BY_STATE_MACHINE, "r")
     res = tmp.read()
     tmp.close()
     assert (res == "start, state, "), "start with valid config failed"
-    os.remove("/tmp/rafcon_unit_tests/test_start_script.txt")
+    os.remove(FILE_MODIFIED_BY_STATE_MACHINE)
 
 '''
 def test_start_script_invalid_config(caplog):
