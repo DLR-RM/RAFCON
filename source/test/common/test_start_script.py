24010ea303cb9fdd0b66680ee2760df12a1b2c8e
Rico Belder <rico.belder@dlr.de>
2016-08-01 12:29:07 +0200

    start_script test: avoid to use shell=True flag - to give it a try - and it seems to more clean

--- source/test/common/test_start_script.py
+++ source/test/common/test_start_script.py
@@ -8,12 +8,23 @@ import testing_utils
 # /tmp/rafcon/unittests/test_start_script.txt??? /tmp/rafcon_unit_test/test_start_script.txt
 
 
+def print_lock_for_Popen(p):
+    out, err = p.communicate()
+    for elem in out.split('\n'):
+        print '\t' + elem
+
+
 def test_start_script_open():
     script = dirname(realpath(rafcon.__file__)) + "/statemachine/start.py"
     start_path = rafcon.__path__[0] + "/../test_scripts/unit_test_state_machines/start_script_test"
-    cmd = sys.executable + " %s -o %s" % (script, start_path)
-    cmd_res = subprocess.call(cmd, shell=True)
-    assert cmd_res == 0
+    # cmd = sys.executable + " %s -o %s" % (script, start_path)
+    # cmd_res = subprocess.call(cmd, shell=True)
+    # assert cmd_res == 0
+    cmd = [sys.executable, script, '-o', start_path]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p.wait()
+    assert p.returncode == 0
+    print_lock_for_Popen(p)
     tmp_file = open("/tmp/rafcon_unit_tests/test_start_script.txt", "r")
     res = tmp_file.read()
     tmp_file.close()
@@ -25,9 +36,14 @@ def test_start_script_state():
     script = dirname(realpath(rafcon.__file__)) + "/statemachine/start.py"
     start_path = rafcon.__path__[0] + "/../test_scripts/unit_test_state_machines/start_script_test"
     state_path = "UTUOSC/AHWBOG"
-    cmd = sys.executable + " %s -o %s -s %s" % (script, start_path, state_path)
-    cmd_res = subprocess.call(cmd, shell=True)
-    assert cmd_res == 0
+    # cmd = sys.executable + " %s -o %s -s %s" % (script, start_path, state_path)
+    # cmd_res = subprocess.call(cmd, shell=True)
+    # assert cmd_res == 0
+    cmd = [sys.executable, script, '-o', start_path, '-s', state_path]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p.wait()
+    assert p.returncode == 0
+    print_lock_for_Popen(p)
     tmp_file = open("/tmp/rafcon_unit_tests/test_start_script.txt", "r")
     res = tmp_file.read()
     tmp_file.close()
@@ -40,15 +56,19 @@ def test_start_script_valid_config():
     script = dirname(realpath(rafcon.__file__)) + "/statemachine/start.py"
     start_path = rafcon.__path__[0] + "/../test_scripts/unit_test_state_machines/start_script_test"
     config = rafcon.__path__[0] + "/../test/common/configs_for_start_script_test/valid_config"
-    cmd = sys.executable + " %s -o %s -c %s" % (script, start_path, config)
-    cmd_res = subprocess.call(cmd, shell=True)
-    assert cmd_res == 0
+    # cmd = sys.executable + " %s -o %s -c %s" % (script, start_path, config)
+    # cmd_res = subprocess.call(cmd, shell=True)
+    # assert cmd_res == 0
+    cmd = [sys.executable, script, '-o', start_path, '-c', config]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p.wait()
+    assert p.returncode == 0
+    print_lock_for_Popen(p)
     tmp = open("/tmp/rafcon_unit_tests/test_start_script.txt", "r")
     res = tmp.read()
     tmp.close()
     assert (res == "start, state, "), "start with valid config failed"
     os.remove("/tmp/rafcon_unit_tests/test_start_script.txt")
-
 
 '''
 def test_start_script_invalid_config(caplog):
@@ -68,6 +88,5 @@ def test_start_script_invalid_config(caplog):
 
 if __name__ == '__main__':
     # test_start_script_state(None)
+    # test_start_script_valid_config()
     pytest.main([__file__])
-
-
