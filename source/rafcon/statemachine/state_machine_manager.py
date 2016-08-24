38687cbe51e667e214f92b9ff122b9e5adf1d578
Rico Belder <rico.belder@dlr.de>
2016-08-24 14:26:12 +0200

    refactor refresh state machines in menu bar and state machine manager for reuse

--- source/rafcon/statemachine/state_machine_manager.py
+++ source/rafcon/statemachine/state_machine_manager.py
@@ -43,10 +43,10 @@ class StateMachineManager(Observable):
         for sm_id in sm_ids:
             self.remove_state_machine(sm_id)
 
-    def refresh_state_machines(self, sm_ids, state_machine_id_to_path):
+    def open_state_machines(self, sm_ids, state_machine_path_by_sm_id):
         from rafcon.statemachine.storage import storage
-        for sm_idx in range(len(state_machine_id_to_path)):
-            state_machine = storage.load_state_machine_from_path(state_machine_id_to_path[sm_ids[sm_idx]])
+        for sm_idx in range(len(state_machine_path_by_sm_id)):
+            state_machine = storage.load_state_machine_from_path(state_machine_path_by_sm_id[sm_ids[sm_idx]])
             self.add_state_machine(state_machine)
 
     def get_sm_id_for_root_state_id(self, root_state_id):
