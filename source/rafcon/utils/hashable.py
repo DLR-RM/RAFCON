93bff71674d6130e53acdaec0ddeb69f0cbf9f6b
Michael Vilzmann <michael.vilzmann@dlr.de>
2016-09-13 09:11:26 +0200

    Add docstrings to utils.Hashable

--- source/rafcon/utils/hashable.py
+++ source/rafcon/utils/hashable.py
@@ -3,19 +3,32 @@ import hashlib
 
 class Hashable(object):
     @staticmethod
-    def update_hash_from_dict(hash, value):
+    def update_hash_from_dict(obj_hash, value):
+        """Updates an existing hash object with another Hashable or a dictionary
+
+        :param obj_hash: The hash object (see Python hashlib documentation)
+        :param value: The value that should be added to the hash (can be another Hashable or a dictionary)
+        """
         if isinstance(value, Hashable):
-            value.update_hash(hash)
+            value.update_hash(obj_hash)
         elif isinstance(value, dict):
             for v in value.itervalues():
-                Hashable.update_hash_from_dict(hash, v)
+                Hashable.update_hash_from_dict(obj_hash, v)
         else:
-            hash.update(str(value))
+            obj_hash.update(str(value))
 
     def update_hash(self, obj_hash):
+        """Should be implemented by derived classes to update the hash with their data fields
+
+        :param obj_hash: The hash object (see Python hashlib)
+        """
         raise NotImplementedError()
 
-    def mutable_hash(self):
-        obj_hash = hashlib.sha256()
+    def mutable_hash(self, obj_hash=hashlib.sha256()):
+        """Creates a hash with the (im)mutable data fields of the object
+
+        :param obj_hash: The hash object (see Python hashlib)
+        :return: The updated hash object
+        """
         self.update_hash(obj_hash)
         return obj_hash
