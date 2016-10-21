83da2013689e9d562b088d377afa3eda45a90317
Lukas Becker <lukas.becker@dlr.de>
2016-10-21 10:45:22 +0200

    added test statemachine
    added test statemachine for the run_to_selected_state method in state_machine_execution_engine.
    it writes a to a file in /tmp/ for every execution state it runs.

---
+++ source/test_scripts/unit_test_state_machines/run_to_selected_state_test/VVBPOY/XSOZVZ/script.py
@@ -0,0 +1,7 @@
+
+def execute(self, inputs, outputs, gvm):
+    with open('/tmp/test_file', 'w') as f:
+        f.write("")
+    with open('/tmp/test_file', 'a') as f:
+        f.write("'" + self.state_id + " wrote to file!'\n")
+    return 0
