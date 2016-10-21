a36391160e3d5e3737af908eccc38f7264385dc6
Lukas Becker <lukas.becker@dlr.de>
2016-10-21 11:08:18 +0200

    Fixed spelling and formatting errors

--- source/test/common/test_run_to_selected_state.py
+++ source/test/common/test_run_to_selected_state.py
@@ -1,8 +1,3 @@
-# core elements
-from rafcon.statemachine.execution.state_machine_execution_engine import StateMachineExecutionEngine
-from rafcon.statemachine.states.execution_state import ExecutionState
-from rafcon.statemachine.states.hierarchy_state import HierarchyState
-
 # singleton elements
 import rafcon.statemachine.singleton
 from rafcon.statemachine.storage import storage
@@ -10,7 +5,6 @@ from rafcon.statemachine.storage import storage
 
 # test environment elements
 import testing_utils
-import pytest
 from rafcon.statemachine.enums import StateExecutionState
 from rafcon.utils import log
 import time
@@ -23,27 +17,27 @@ def test_run_to_selected_state(caplog):
     testing_utils.test_multithrading_lock.acquire()
 
     sm = storage.load_state_machine_from_path("/home_local/stoertebeker/develop/rafcon_tests/run_selected_state")
-    #select state machine for this purpose
-    
+    # select state machine for this purpose
+
     rafcon.statemachine.singleton.state_machine_manager.add_state_machine(sm)
     rafcon.statemachine.singleton.state_machine_execution_engine.run_to_selected_state("VVBPOY/AOZXRY", sm.state_machine_id)
-    #run the statmachine to the state befor AOYXRY, this is an asychronous task
+    # run the statemachine to the state before AOYXRY, this is an asychronous task
 
     while not sm.get_state_by_path("VVBPOY/ABNQFK").state_execution_status is StateExecutionState.WAIT_FOR_NEXT_STATE:
         time.sleep(.001)
-    #wait until the statemachine is executed until ABNQFK the state before AOZXRY, so it doesnt check for the file
-    #before its even written
+    # wait until the statemachine is executed until ABNQFK the state before AOZXRY, so it doesnt check for the file
+    # before its even written
 
     with open('/tmp/test_file', 'r') as test_file:
         lines = test_file.readlines()
 
     logger.debug("last entry in file is " + lines[len(lines)-1])
     assert len(lines) < 3
-    #read all lines of the file and check if not more than 2 states have written to it
+    # read all lines of the file and check if not more than 2 states have written to it
 
     rafcon.statemachine.singleton.state_machine_execution_engine.stop()
     rafcon.statemachine.singleton.state_machine_execution_engine.join()
-    #the state machines waits at ABNQFK with state WAIT_FOR_NEXT_STATE, so it needs to be stopped manually
+    # the state machines waits at ABNQFK with state WAIT_FOR_NEXT_STATE, so it needs to be stopped manually
 
     testing_utils.test_multithrading_lock.release()
     testing_utils.assert_logger_warnings_and_errors(caplog)
@@ -51,4 +45,3 @@ def test_run_to_selected_state(caplog):
 
 if __name__ == '__main__':
     test_run_to_selected_state(None)
-    # pytest.main([__file__])
