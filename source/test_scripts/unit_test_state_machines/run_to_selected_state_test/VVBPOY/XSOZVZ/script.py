d03ac764137fa5c4ba7fceb0c7e97dcba020f227
Lukas Becker <lukas.becker@dlr.de>
2016-10-24 19:56:26 +0200

    RAFCON_BASE_PATH fixed

--- source/test_scripts/unit_test_state_machines/run_to_selected_state_test/VVBPOY/XSOZVZ/script.py
+++ source/test_scripts/unit_test_state_machines/run_to_selected_state_test/VVBPOY/XSOZVZ/script.py
@@ -1,16 +1,9 @@
 def execute(self, inputs, outputs, gvm):
-    
+    from rafcon.utils.constants import RAFCON_TEMP_PATH_BASE
     import os
     
-    try:
-        "RAFCON_TEMP_PATH_BASE" in os.environ
-        if not os.environ.get("RAFCON_TEMP_PATH_BASE"):
-            os.environ["RAFCON_TEMP_PATH_BASE"] = '/tmp/'
-    except KeyError:
-        self.logger.error("Enviroment variable path not set")    
-    
-    gvm.set_variable('tmp_path', os.environ["RAFCON_TEMP_PATH_BASE"] + 'test_file')
-    
+    gvm.set_variable('tmp_path', RAFCON_TEMP_PATH_BASE + "/test_file")
+    self.logger.info(RAFCON_TEMP_PATH_BASE)
     with open(str(gvm.get_variable('tmp_path')), 'w') as f:
         f.write("")
     with open(str(gvm.get_variable('tmp_path')), 'a') as f:
