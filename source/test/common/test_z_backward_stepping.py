3bbe41ccecabe8c58e5f72654c7689687bc6fb82
Sebastian Brunner <sebastian.brunner@dlr.de>
2016-10-31 09:01:57 +0100

    quick-fix barrier concurrency state
    as proposed by @Franz S.

--- source/test/common/test_z_backward_stepping.py
+++ source/test/common/test_z_backward_stepping.py
@@ -106,6 +106,7 @@ def trigger_gui_signals_library_state(*args):
 
 
 def test_backward_stepping_library_state(caplog):
+    sys.setrecursionlimit(3000)
     testing_utils.start_rafcon()
     testing_utils.remove_all_libraries()
     # Load test library
@@ -197,6 +198,7 @@ def trigger_gui_signals_preemptive_state(*args):
 
 
 def test_backward_stepping_preemptive_state(caplog):
+    sys.setrecursionlimit(3000)
     testing_utils.start_rafcon()
     #testing_utils.remove_all_libraries()
     gui_config.global_gui_config.set_config_value('HISTORY_ENABLED', False)
@@ -280,6 +282,7 @@ def trigger_gui_signals_barrier_state(*args):
 
 
 def test_backward_stepping_barrier_state(caplog):
+    sys.setrecursionlimit(3000)
     testing_utils.start_rafcon()
     testing_utils.remove_all_libraries()
     gui_config.global_gui_config.set_config_value('HISTORY_ENABLED', False)
