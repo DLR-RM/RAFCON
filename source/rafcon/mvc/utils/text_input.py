08022035f64f797c7aef5cb9c365ab95b447b751
Lukas Becker <lukas.becker@dlr.de>
2016-11-08 10:37:54 +0100

    Source Editor: Generalisation of text input window
    This is mainly commited to have the current code on the git

--- source/rafcon/mvc/utils/text_input.py
+++ source/rafcon/mvc/utils/text_input.py
@@ -1,40 +1,42 @@
 import gtk
-import os
+
 from rafcon.mvc.utils import constants
 from rafcon.utils import log
 from rafcon.mvc.config import global_gui_config
 
 logger = log.get_logger(__name__)
 
+
 class RAFCONTextInput(gtk.Window):
 
-    def __init__(self):
+    def __init__(self, content='', window_title=''):
         super(RAFCONTextInput, self).__init__()
+        self.content = content
+        self.window_title = window_title
 
-    def enter_callback(self, widget, entry):
-        entry_text = entry.get_text()
+    def return_text(self):
+        return self.content
 
-    def set_path(self, widget, entry, text="gedit"):
-        logger.debug(entry.get_text() + " was set as external editor")
+    def update(self, widget, entry, text="gedit"):
+        logger.info("'{}' was set as external editor".format(entry.get_text()))
+        self.content = entry.get_text
         global_gui_config.set_config_value('DEFAULT_EXTERNAL_EDITOR', entry.get_text())
         self.destroy()
         return
 
-    def setup(self, title, entry_sample_text):
-        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        self.set_size_request(400,100)
-        self.set_title(title)
+    def setup(self):
+
+        self.set_size_request(400, 100)
+        self.set_title(self.window_title)
 
         vbox = gtk.VBox(spacing=constants.GRID_SIZE, homogeneous=False)
         self.add(vbox)
 
-        vbox.show()
-
         entry = gtk.Entry()
 
         entry.set_max_length(40)
-        entry.connect("activate", self.enter_callback, entry)
-        entry.set_text(entry_sample_text)
+        entry.connect("activate", self.update, entry)
+        entry.set_text(self.content)
         entry.set_editable(1)
         entry.select_region(0, len(entry.get_text()))
 
@@ -45,22 +47,19 @@ class RAFCONTextInput(gtk.Window):
         hbox = gtk.HBox(spacing=constants.GRID_SIZE, homogeneous=False)
         vbox.add(hbox)
 
-        hbox.show()
-
         button = gtk.Button(stock=gtk.STOCK_OK)
-        button.connect("clicked", self.set_path, entry)
+        button.connect("clicked", self.update, entry)
         hbox.pack_start(button, True, True, 0)
-        button.set_flags(gtk.CAN_DEFAULT)
-        button.grab_default()
         button.show()
 
-        button2 = gtk.Button(stock=gtk.STOCK_CANCEL)
-        button2.connect("clicked", lambda w: self.destroy())
-        hbox.pack_start(button2, True, True, 0)
-        button2.set_flags(gtk.CAN_DEFAULT)
-        button2.grab_default()
-        button2.show()
+        button_cancel = gtk.Button(stock=gtk.STOCK_CANCEL)
+        button_cancel.connect("clicked", lambda w: self.destroy())
+        hbox.pack_start(button_cancel, True, True, 0)
+        button_cancel.show()
 
+        vbox.show()
+        hbox.show()
+        hbox.show()
         self.show()
-
+        #return entry.get_text()
 
