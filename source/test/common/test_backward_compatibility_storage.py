9fb6f943b2f63b1bb3f3a85f6ca1b3e6692ac142
Franz Steinmetz <franz.steinmetz@dlr.de>
2016-12-12 12:42:08 +0100

    Improve backward compatibility test
    - check all state machines within folder
    - run test in main thread
    - use new helper methods

--- source/test/common/test_backward_compatibility_storage.py
+++ source/test/common/test_backward_compatibility_storage.py
@@ -1,86 +1,75 @@
-import gtk
-import threading
-import pytest
-import time
 import os
-import sys
 
 # gui elements
 import rafcon.gui.config as gui_config
 import rafcon.gui.singleton
-from rafcon.gui.controllers.main_window import MainWindowController
-from rafcon.gui.views.main_window import MainWindowView
 
 # core elements
 import rafcon.core.config
-import rafcon.core.singleton
-from rafcon.core.singleton import state_machine_execution_engine
+import rafcon.core.singleton as singletons
 
 # general tool elements
 from rafcon.utils import log
 import testing_utils
-from testing_utils import call_gui_callback
+from testing_utils import call_gui_callback, run_gui, wait_for_gui_quit
 
 logger = log.get_logger(__name__)
 
 
-def trigger_gvm_signals(main_window_controller):
+def run_state_machines(state_machines_path):
+    from rafcon.gui.singleton import main_window_controller
     gvm = rafcon.core.singleton.global_variable_manager
-    gvm_controller = main_window_controller.get_controller('global_variable_manager_ctrl')
     menubar_ctrl = main_window_controller.get_controller('menu_bar_controller')
+    execution_engine = singletons.state_machine_execution_engine
+    state_machine_manager = singletons.state_machine_manager
 
-    # TODO: replace these at some point with released state machines
-    call_gui_callback(menubar_ctrl.on_open_activate, None, None, os.path.join(
-        testing_utils.RAFCON_PATH, "../test_scripts/unit_test_state_machines/0.7.13.backward_compatibility_storage_test"))
+    for state_machine_folder in os.listdir(state_machines_path):
+        state_machine_path = os.path.join(state_machines_path, state_machine_folder)
 
-    state_machine_execution_engine.start()
+        if not execution_engine.finished_or_stopped():
+            raise RuntimeError("The execution engine is not stopped")
 
-    while not state_machine_execution_engine.finished_or_stopped():
-        time.sleep(0.01)
+        print "Loading state machine from path: {}".format(state_machine_path)
+        call_gui_callback(menubar_ctrl.on_open_activate, None, None, state_machine_path)
 
-    assert gvm.get_variable("b1") == 1
-    assert gvm.get_variable("b2") == 1
-    assert gvm.get_variable("h1") == 1
-    assert gvm.get_variable("e1") == 1
-    assert gvm.get_variable("l1") == 1
+        testing_utils.remove_all_gvm_variables()
 
-    call_gui_callback(menubar_ctrl.on_quit_activate, None, None, True)
+        execution_engine.start()
+        if not execution_engine.join(3):
+            raise RuntimeError("State machine did not finish within the given time")
+
+        assert gvm.get_variable("b1") == 1
+        assert gvm.get_variable("b2") == 1
+        assert gvm.get_variable("h1") == 1
+        assert gvm.get_variable("e1") == 1
+        assert gvm.get_variable("l1") == 1
+
+        state_machine_manager.remove_state_machine(state_machine_manager.active_state_machine_id)
 
 
 def test_backward_compatibility_storage(caplog):
-    sys.setrecursionlimit(3000)
-    testing_utils.start_rafcon()
+    path = testing_utils.get_test_sm_path(os.path.join("unit_test_state_machines", "backward_compatibility"))
 
-    gui_config.global_gui_config.set_config_value('HISTORY_ENABLED', False)
-    gui_config.global_gui_config.set_config_value('AUTO_BACKUP_ENABLED', False)
+    run_gui(None, {
+                    'HISTORY_ENABLED': False,
+                    'AUTO_BACKUP_ENABLED': False
+                  },
+                  {
+                    'unit_test_state_machines': testing_utils.get_test_sm_path("unit_test_state_machines")
+                  })
 
-    testing_utils.remove_all_libraries()
-    library_paths = rafcon.core.config.global_config.get_config_value("LIBRARY_PATHS")
-    library_paths["unit_test_state_machines"] = testing_utils.RAFCON_PATH + "/../test_scripts/unit_test_state_machines"
-    rafcon.core.singleton.library_manager.refresh_libraries()
+    try:
+        run_state_machines(path)
+    finally:
+        from rafcon.gui.singleton import main_window_controller
+        menubar_ctrl = main_window_controller.get_controller('menu_bar_controller')
+        call_gui_callback(menubar_ctrl.on_quit_activate, None, None, True)
 
-    testing_utils.remove_all_gvm_variables()
-
-    testing_utils.sm_manager_model = rafcon.gui.singleton.state_machine_manager_model
-
-    main_window_view = MainWindowView()
-    main_window_controller = MainWindowController(testing_utils.sm_manager_model, main_window_view)
-
-    # Wait for GUI to initialize
-    testing_utils.wait_for_gui()
-
-    thread = threading.Thread(target=trigger_gvm_signals, args=[main_window_controller])
-    thread.start()
-    gtk.main()
+    wait_for_gui_quit()
     logger.debug("after gtk main")
 
-    thread.join()
-
     testing_utils.remove_all_libraries()
-
     testing_utils.test_multithreading_lock.release()
-
-    # expected_errors=1 because global_variable_is_editable throws an error
     testing_utils.assert_logger_warnings_and_errors(caplog)
 
 
