90e6f142e4e1c9e4f45fe054de7f22ce4c78d876
Sebastian Brunner <sebastian.brunner@dlr.de>
2016-12-16 15:24:37 +0100

    fix cata port comparison
    in data port list store

--- source/rafcon/gui/utils/comparison.py
+++ source/rafcon/gui/utils/comparison.py
@@ -11,9 +11,9 @@ def compare_variables(tree_model, iter1, iter2, user_data=None):
         path1 = tree_model.get_path(iter1)[0]
         path2 = tree_model.get_path(iter2)[0]
         # get key of first variable
-        name1 = tree_model[path1][0][0]
+        name1 = tree_model[path1][0]
         # get key of second variable
-        name2 = tree_model[path2][0][0]
+        name2 = tree_model[path2][0]
         name1_as_bits = ' '.join(format(ord(x), 'b') for x in name1)
         name2_as_bits = ' '.join(format(ord(x), 'b') for x in name2)
         if name1_as_bits == name2_as_bits:
