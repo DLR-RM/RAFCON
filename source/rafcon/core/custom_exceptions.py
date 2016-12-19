b817c925b924219e26d7c1cadff4dd53bf7f2a69
Sebastian Brunner <sebastian.brunner@dlr.de>
2016-12-19 10:54:34 +0100

    add support for changing library interfaces
    the LIBRARY_RECOVERY_MODE = True allows a user to load faulty state machines, in which libraries changed their interfaces
    the following changes are supported:
    - wrong data type
    - missing data port (input and output)
    - missing outcome

--- source/rafcon/core/custom_exceptions.py
+++ source/rafcon/core/custom_exceptions.py
@@ -20,3 +20,32 @@ class LibraryNotFoundException(Exception):
 
         # Call the base class constructor with the parameters it needs
         super(LibraryNotFoundException, self).__init__(message)
+
+
+class RecoveryModeException(Exception):
+
+    def __init__(self, message, do_delete_item):
+        """ A custom exception for the case when a state machine cannot be loaded because the interface of a
+        library state changed (e.g. a data port has another data type, or a data port/outcome is completely missing)
+
+        :param message: the error message for the exception
+        :param do_delete_item: a flag to indicate if the error is critical and the state element has to be deleted
+        :return:
+        """
+
+        self._do_delete_item = do_delete_item
+
+        # Call the base class constructor with the parameters it needs
+        super(RecoveryModeException, self).__init__(message)
+
+    @property
+    def do_delete_item(self):
+        """Property for the do_delete_item field
+
+        """
+        return self._do_delete_item
+
+    @do_delete_item.setter
+    def do_delete_item(self, do_delete_item):
+        self._do_delete_item = do_delete_item
+
