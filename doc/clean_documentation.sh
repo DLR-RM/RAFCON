1578b456c271447337eeefe34d26a058f1148650
Franz Steinmetz <franz.steinmetz@dlr.de>
2016-12-20 11:08:23 +0100

    Documentation: Adapt clean_documentation script

--- doc/clean_documentation.sh
+++ doc/clean_documentation.sh
@@ -1,7 +1,1 @@
-rm -r documentation/documentation/
-rm -r documentation/_sources
-rm -r documentation/_static
-rm documentation/*.html
-rm documentation/objects.inv
-rm documentation/*.js
-rm -r documentation/.doctrees
+rm -rf build_doc
