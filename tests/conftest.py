c219d70c349dd460423f5a370dba857fdd2dda37
Franz Steinmetz <franz.steinmetz@dlr.de>
2017-01-13 15:49:08 +0100

    Recreate temp test path after deletion
    The RAFCON_TEMP_PATH_TEST_BASE of the tests is deleted after all
    tests have been performed. With this commit, the folder itself (not
    its content) is created after the deletion, so that it is existing if
    the test suite is started another time.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,11 @@ def pytest_unconfigure(config):
 
 def clean_temp_test_directory():
     import shutil
+    import os
     import testing_utils
     test_temp_path = testing_utils.RAFCON_TEMP_PATH_TEST_BASE
     try:
         shutil.rmtree(test_temp_path)
+        os.mkdir(test_temp_path)
     except OSError:
         pass
