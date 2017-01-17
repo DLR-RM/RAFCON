496d6674f58ffbfdd3962c95d722c5ca3f917f57
Franz Steinmetz <franz.steinmetz@dlr.de>
2017-01-17 17:07:14 +0100

    Tests: new subtest for income pos conversion

--- tests/common/test_editor_meta_data_conversion.py
+++ tests/common/test_editor_meta_data_conversion.py
@@ -16,6 +16,14 @@ def test_state_rel_pos(use_gaphas):
     assert meta_data["rel_pos"] == (1, -2)
 
 
+def test_state_income():
+    state = State()
+    state_m = StateModel(state, parent=None)
+    state_m.meta["gui"]["editor_opengl"]["size"] = (50, 50)
+    meta_data = state_m.get_meta_data_editor(for_gaphas=True)
+    assert meta_data["income"]["rel_pos"] == (0, 25)
+
+
 @pytest.mark.parametrize("use_gaphas", [False, True])
 def test_transition_waypoints(use_gaphas):
     transition = Transition(None, 0, None, 0, None)
