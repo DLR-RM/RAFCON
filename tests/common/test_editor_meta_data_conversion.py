b8f6f8ee8bcad94f9e627277d0d6db90f4e54572
Franz Steinmetz <franz.steinmetz@dlr.de>
2017-01-18 16:12:40 +0100

    Test: check outcome conversion

--- tests/common/test_editor_meta_data_conversion.py
+++ tests/common/test_editor_meta_data_conversion.py
@@ -24,6 +24,43 @@ def test_state_income():
     assert meta_data["income"]["rel_pos"] == (0, 25)
 
 
+def test_state_3_outcomes():
+    state = State()
+    state_m = StateModel(state, parent=None)
+    state_m.meta["gui"]["editor_opengl"]["size"] = (60, 60)
+    state_m.get_meta_data_editor(for_gaphas=True)
+
+    for outcome_m in state_m.outcomes:
+        outcome = outcome_m.outcome
+        outcome_pos = outcome_m.get_meta_data_editor()["rel_pos"]
+        if outcome.outcome_id == -1:
+            assert outcome_pos == (54, 0)
+        elif outcome.outcome_id == -2:
+            assert outcome_pos == (42, 0)
+        else:
+            assert outcome_pos == (60, 30)
+
+
+def test_state_4_outcomes():
+    state = State()
+    state.add_outcome("success2")
+    state_m = StateModel(state, parent=None)
+    state_m.meta["gui"]["editor_opengl"]["size"] = (90, 90)
+    state_m.get_meta_data_editor(for_gaphas=True)
+
+    for outcome_m in state_m.outcomes:
+        outcome = outcome_m.outcome
+        outcome_pos = outcome_m.get_meta_data_editor()["rel_pos"]
+        if outcome.outcome_id == -1:
+            assert outcome_pos == (81, 0)
+        elif outcome.outcome_id == -2:
+            assert outcome_pos == (63, 0)
+        elif outcome.outcome_id == 0:
+            assert outcome_pos == (90, 30)
+        else:
+            assert outcome_pos == (90, 60)
+
+
 @pytest.mark.parametrize("use_gaphas", [False, True])
 def test_transition_waypoints(use_gaphas):
     transition = Transition(None, 0, None, 0, None)
