2f0bc89e113aa7c3ced8deadb97be2eba1c30003
Rico Belder <rico.belder@dlr.de>
2017-02-10 17:26:53 +0100

    fix last renaming from statemachine to core sub-package and mvc to gui

--- source/rafcon/__init__.py
+++ source/rafcon/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["mvc", "statemachine", "utils"]
+__all__ = ["gui", "core", "utils"]
 
 from rafcon.utils.filesystem import read_version_from_pt_file
 __version__ = read_version_from_pt_file()
--- source/rafcon/gui/controllers/menu_bar.py
+++ source/rafcon/gui/controllers/menu_bar.py
@@ -194,7 +194,7 @@ class MenuBarController(ExtendedController):
 
     def on_full_screen_activate(self, *args):
         """
-        function to display the currently selected statemachine in full screen mode
+        function to display the currently selected state machine in full screen mode
         :param args:
         :return:
         """
