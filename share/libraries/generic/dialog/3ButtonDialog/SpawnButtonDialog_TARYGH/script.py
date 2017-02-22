120718c2394755ac665374965318f364e3632f02
Lukas Becker <lukas.becker@dlr.de>
2017-02-22 13:46:25 +0100

    Finish library states
    - Add 2 and 3 Button dialog as they are used often
    - Add a dialog which returns the response id as output variable

---
+++ share/libraries/generic/dialog/3ButtonDialog/SpawnButtonDialog_TARYGH/script.py
@@ -0,0 +1,30 @@
+from rafcon.gui.utils.dialog import RAFCONButtonDialog
+
+def execute(self, inputs, outputs, gvm):
+    self.logger.debug("Creating button dialog")
+    
+    if len(inputs['buttons']) != 3:
+        self.logger.error("Please specify exactly three buttons as a list")
+        return "aborted"
+        
+    dialog_window = RAFCONButtonDialog(markup_text=inputs['message_text'],
+                                      button_texts=inputs['buttons'])
+    abort = inputs['abort_on_quit']
+    
+    response_id = dialog_window.run()
+    
+    dialog_window.destroy()
+    
+    if response_id == 1:
+        return 0
+        
+    elif response_id == 2:
+        return 1
+    
+    elif response_id == 3:
+        return 2
+    
+    if abort: 
+        return "aborted"
+    else:
+        return 1
