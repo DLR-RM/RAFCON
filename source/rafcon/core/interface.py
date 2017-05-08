f4c9500bd2fb0f33afe1ee7dce99dbd8b50903ca
Franz Steinmetz <franz.steinmetz@dlr.de>
2017-05-08 17:06:25 +0200

    Refactor core interface
    - write docstrings
    - return None if query is not successful
    - add option to pass default_path

--- source/rafcon/core/interface.py
+++ source/rafcon/core/interface.py
@@ -16,31 +16,56 @@
 
 """
 
-def open_folder_cmd_line(query):
+import os
+
+
+def open_folder_cmd_line(query, default_path=None):
+    """Queries the user for a path to open
+    
+    :param str query: Query that asks the user for a specific folder path to be opened
+    :param str default_path: Path to use if the user doesn't specify a path 
+    :return: Input path from the user or `default_path` if nothing is specified or None if path does not exist
+    :rtype: str
+    """
     user_input = raw_input(query + ': ')
     if len(user_input) == 0:
+        user_input = default_path
+    if not user_input or not os.path.isdir(user_input):
         return None
     return user_input
 
 open_folder_func = open_folder_cmd_line
 
 
-def create_folder_cmd_line(query):
-    import os
+def create_folder_cmd_line(query, default_path=None):
+    """Queries the user for a path to be created
+    
+    :param str query: Query that asks the user for a specific folder path  to be created
+    :param str default_path: Path to use if the user doesn't specify a path 
+    :return: Input path from the user or `default_path` if nothing is specified or None if directory could ne be created
+    :rtype: str
+    """
     user_input = raw_input(query + ': ')
-    if len(user_input) == 0 or os.path.exists(user_input):
-        return None
+    if len(user_input) == 0:
+        user_input = default_path
 
-    try:
-        os.makedirs(user_input)
-    except OSError:
+    if not user_input:
         return None
+    if not os.path.isdir(user_input):
+        try:
+            os.makedirs(user_input)
+        except OSError:
+            return None
     return user_input
 
 create_folder_func = create_folder_cmd_line
 
 
-def show_notice(query):
-    raw_input(query)
+def show_notice(notice):
+    """Shows a notice on the console that has to be acknowledged 
+    
+    :param str notice: Notice to show to the user 
+    """
+    raw_input(notice)
 
 show_notice_func = show_notice
