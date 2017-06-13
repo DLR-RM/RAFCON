728344b9898ce57f207189cf028ef22f39d6ed76
Rico Belder <rico.belder@dlr.de>
2017-06-13 11:25:35 +0200

    abstaract state model: correct docstring of update_is_start - first stae in a its parent was not within the condition check

--- source/rafcon/gui/models/abstract_state.py
+++ source/rafcon/gui/models/abstract_state.py
@@ -161,8 +161,8 @@ class AbstractStateModel(MetaModel, Hashable):
     def update_is_start(self):
         """Updates the `is_start` property of the state
         
-        A state is a start state, if it is the root state, it has no parent, the parent is a LibraryState or it is 
-        the first state within a ContainerState.
+        A state is a start state, if it is the root state, it has no parent, the parent is a LibraryState or the state's
+        state_id is identical with the ContainerState.start_state_id of the ContainerState it is within.
         """
         self.is_start = self.state.is_root_state or \
                         self.parent is None or \
