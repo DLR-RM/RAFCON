c79ef117d1095d857bbc2e50b58ba10bc05b3fef
Rico Belder <rico.belder@dlr.de>
2017-08-02 11:44:26 +0200

    session restore: full state selection restore of all open state machines - only state selection is restored - refactor tab and state machine selection structure

--- source/rafcon/gui/backup/session.py
+++ source/rafcon/gui/backup/session.py
@@ -30,34 +30,34 @@ def store_session():
     state_machines_editor_ctrl = main_window_controller.get_controller('state_machines_editor_ctrl')
     number_of_pages = state_machines_editor_ctrl.view['notebook'].get_n_pages()
     selected_page_number = None
-    ordered_sm_ids = []
+    list_of_tab_meta = []
     for page_number in range(number_of_pages):
         page = state_machines_editor_ctrl.view['notebook'].get_nth_page(page_number)
-        ordered_sm_ids.append(state_machines_editor_ctrl.get_state_machine_id_for_page(page))
-        if ordered_sm_ids[-1] == state_machine_manager_model.selected_state_machine_id:
+        state_machines_editor_ctrl.get_state_machine_id_for_page(page)
+        sm_id = state_machines_editor_ctrl.get_state_machine_id_for_page(page)
+        if sm_id == state_machine_manager_model.selected_state_machine_id:
             selected_page_number = page_number
 
-    # backup state machine selection
-    selection_of_selected_sm = []
-    if selected_page_number is not None:
-        for model in state_machine_manager_model.get_selected_state_machine_model().selection.get_all():
+        # backup state machine selection
+        selection_of_sm = []
+        for model in state_machine_manager_model.state_machines[sm_id].selection.get_all():
             if isinstance(model, AbstractStateModel):
                 # TODO extend to full range of selection -> see core_identifier action-module
-                selection_of_selected_sm.append(model.state.get_path())
+                selection_of_sm.append(model.state.get_path())
 
-    # store final state machine backup meta data to backup session tabs for the next run
-    list_of_tab_meta = [state_machine_manager_model.state_machines[sm_id].auto_backup.meta for sm_id in ordered_sm_ids]
+        list_of_tab_meta.append({'backup_meta': state_machine_manager_model.state_machines[sm_id].auto_backup.meta,
+                                 'selection': selection_of_sm})
+
+    # store final state machine backup meta data to backup session tabs and selection for the next run
     global_runtime_config.set_config_value('open_tabs', list_of_tab_meta)
-    # store state-machines-editor- and selected-state-machine-selection
-    global_runtime_config.set_config_value('selection', {'state_machine_page_number': selected_page_number,
-                                                         'selection_of_selected_sm': selection_of_selected_sm})
+    global_runtime_config.set_config_value('selected_state_machine_page_number', selected_page_number)
 
 
 def reset_session():
     """Remove all information from the current session """
     from rafcon.gui.singleton import global_runtime_config
     global_runtime_config.set_config_value('open_tabs', [])
-    global_runtime_config.set_config_value('selection', {})
+    global_runtime_config.set_config_value('selected_state_machine_page_number', None)
 
 
 def restore_session_from_runtime_config():
@@ -79,9 +79,12 @@ def restore_session_from_runtime_config():
         logger.info("No session found for recovery")
         return
 
-    # load and recover state machines like they were opened before
-    for idx, backup_meta_dict in enumerate(open_tabs):
+    # load and restore state machines like they were opened before
+    open_sm = []
+    for idx, tab_meta_dict in enumerate(open_tabs):
+        backup_meta_dict = tab_meta_dict['backup_meta']
         from_backup_path = None
+        open_sm.append(None)
         # TODO do this decision before storing or maybe store the last stored time in the auto backup?!
         # pick folder name dependent on time, and backup meta data existence
         # problem is that the backup time is maybe not the best choice
@@ -113,7 +116,7 @@ def restore_session_from_runtime_config():
         if from_backup_path is not None:
             # open state machine, recover mark dirty flags, cleans dirty lock files
             logger.info("Restoring from backup {0}".format(from_backup_path))
-            recover_state_machine_from_backup(from_backup_path)
+            state_machine_m = recover_state_machine_from_backup(from_backup_path)
         else:
             if 'last_saved' not in backup_meta_dict or backup_meta_dict['last_saved']['file_system_path'] is None:
                 continue
@@ -125,6 +128,9 @@ def restore_session_from_runtime_config():
             # logger.info("backup from last saved", path, sm_meta_dict)
             state_machine = storage.load_state_machine_from_path(path)
             state_machine_manager_model.state_machine_manager.add_state_machine(state_machine)
+            state_machine_m = state_machine_manager_model.state_machines[state_machine.state_machine_id]
+
+        open_sm[idx] = state_machine_m
 
     global_runtime_config.extend_recently_opened_by_current_open_state_machines()
 
@@ -133,25 +139,32 @@ def restore_session_from_runtime_config():
         while gtk.events_pending():
             gtk.main_iteration(False)
 
-    # restore backup-ed selection
-    session_selection = global_runtime_config.get_config_value('selection', None)
-    if session_selection and session_selection['state_machine_page_number'] is not None:
-        selected_state_machine_page_number = session_selection['state_machine_page_number']
+    # restore all state machine selections separate to avoid states-editor and state editor creation problems
+    for idx, tab_meta_dict in enumerate(open_tabs):
+        state_machine_m = open_sm[idx]
+        if state_machine_m is None:  # state machine could not be open
+            return
+
+        # restore state machine selection
+        selected_model_set = []
+        for core_element_identifier in tab_meta_dict['selection']:
+            selected_model_set.append(state_machine_m.get_state_model_by_path(core_element_identifier))
+        state_machine_m.selection.set(selected_model_set)
+
+    # restore backup-ed tab selection
+    selected_page_number = global_runtime_config.get_config_value('selected_state_machine_page_number', None)
+    if selected_page_number is not None:
+        selected_state_machine_page_number = selected_page_number
         if selected_state_machine_page_number is None:
             return
         state_machines_editor_ctrl = main_window_controller.get_controller('state_machines_editor_ctrl')
-        if not state_machines_editor_ctrl.view['notebook'].get_n_pages() >= selected_state_machine_page_number:
+        if not state_machines_editor_ctrl.view['notebook'].get_n_pages() >= selected_page_number:
             logger.warning("Page id {0} does not exist so session restore can not re-create selection."
-                           "".format(selected_state_machine_page_number))
+                           "".format(selected_page_number))
             return
         notebook = state_machines_editor_ctrl.view['notebook']
-        page = state_machines_editor_ctrl.on_switch_page(notebook, None, selected_state_machine_page_number)
+        page = state_machines_editor_ctrl.on_switch_page(notebook, None, selected_page_number)
         selected_sm_id = state_machine_manager_model.selected_state_machine_id
         if not selected_sm_id == state_machines_editor_ctrl.get_state_machine_id_for_page(page):
-            logger.warning("Selection was not set correctly so session restore can not re-create selection.")
+            logger.warning("Selection of page was not set correctly so session restore can not re-create selection.")
             return
-        selected_sm_m = state_machine_manager_model.get_selected_state_machine_model()
-        selected_model_set = []
-        for core_element_identifier in session_selection['selection_of_selected_sm']:
-            selected_model_set.append(selected_sm_m.get_state_model_by_path(core_element_identifier))
-        selected_sm_m.selection.set(selected_model_set)
