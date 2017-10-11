b8bc2d605c5b24d7cf34780aedd1abbce91f8dc8
Rico Belder <rico.belder@dlr.de>
2017-10-11 14:25:04 +0200

    fix and refactor logging console
    The controller is now observing the gui config model
    which is handed by the main window controller.
    The set of enables is updated only if needed.
    Enables updates are preformed by controller no more by the view.
    The main window controller only updates the gui config.
    Main window controller save changes of enables by saving to config file.

--- source/rafcon/gui/controllers/logging_console.py
+++ source/rafcon/gui/controllers/logging_console.py
@@ -15,6 +15,8 @@
 import gtk
 import threading
 
+from rafcon.gui.models.config_model import ConfigModel
+from rafcon.gui.views.logging_console import LoggingConsoleView
 from rafcon.gui.controllers.utils.extended_controller import ExtendedController
 from rafcon.utils import log, log_helpers
 logger = log.get_logger(__name__)
@@ -23,34 +25,26 @@ logger = log.get_logger(__name__)
 class LoggingConsoleController(ExtendedController):
     """Controller handling the updates and modifications of the logging console.
 
-    :param
-    :param rafcon.mvc.views.logging.LoggingView view: The GTK view showing the source editor.
+    :param rafcon.gui.models.config_model.ConfigModel: Gui config model holding and observing the global gui config.
+    :param rafcon.gui.views.logging_console.LoggingConsoleView view: The GTK view showing the logging messages.
     """
 
     def __init__(self, model, view):
-        ExtendedController.__init__(self, model, view)
+        assert isinstance(model, ConfigModel)
+        assert isinstance(view, LoggingConsoleView)
+        super(LoggingConsoleController, self).__init__(model, view)
 
         self._lock = threading.Lock()
         self._log_entries = []
-
+        self._enables = self._get_config_enables()
         log_helpers.LoggingViewHandler.add_logging_view('main', self)
 
     def register_view(self, view):
         view.text_view.connect('populate_popup', self.add_clear_menu_item)
-
+        self.view.set_enables(self._enables)
         self.update_filtered_buffer()
 
     def register_adapters(self):
-        pass
-
-    def register_actions(self, shortcut_manager):
-        """Register callback methods for triggered actions
-
-        :param rafcon.mvc.shortcut_manager.ShortcutManager shortcut_manager: Shortcut Manager Object holding mappings
-            between shortcuts and actions.
-        """
-        # shortcut_manager.add_callback_for_action("copy", self._copy)
-        # shortcut_manager.add_callback_for_action("paste", self._paste)
         pass
 
     def print_message(self, message, log_level, new=True):
@@ -74,8 +68,6 @@ class LoggingConsoleController(ExtendedController):
     def update_filtered_buffer(self):
         if self.view is None:
             return
-        self.view.read_enables()
-
         self.print_filtered_buffer()
 
         self.view.text_view.scroll_mark_onscreen(self.view.text_view.get_buffer().get_insert())
@@ -91,3 +83,29 @@ class LoggingConsoleController(ExtendedController):
         menu.append(gtk.SeparatorMenuItem())
         menu.append(clear_item)
         menu.show_all()
+
+    def _get_config_enables(self):
+        keys = ['INFO', 'DEBUG', 'WARNING', 'ERROR']
+        return {key: self.model.config.get_config_value('LOGGING_SHOW_' + key, True) for key in keys}
+
+    @ExtendedController.observe("config", after=True)
+    def model_changed(self, model, prop_name, info):
+        """ React to configuration changes """
+        current_enables = self._get_config_enables()
+        if not self._enables == current_enables:
+            self._enables = current_enables
+            self.view.set_enables(self._enables)
+            self.update_filtered_buffer()
+
+    def _get_config_enables(self):
+        keys = ['INFO', 'DEBUG', 'WARNING', 'ERROR']
+        return {key: self.model.config.get_config_value('LOGGING_SHOW_' + key, True) for key in keys}
+
+    @ExtendedController.observe("config", after=True)
+    def model_changed(self, model, prop_name, info):
+        """ React to configuration changes """
+        current_enables = self._get_config_enables()
+        if not self._enables == current_enables:
+            self._enables = current_enables
+            self.view.set_enables(self._enables)
+            self.update_filtered_buffer()
