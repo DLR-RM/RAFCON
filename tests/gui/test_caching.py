5ad3641c8b30a0e0dd4c556ee4af2a07652098cf
Franz Steinmetz <franz.steinmetz@dlr.de>
2018-01-09 16:20:55 +0100

    fix(test_caching): Adapt test to changes in ValueCache

--- tests/gui/test_caching.py
+++ tests/gui/test_caching.py
@@ -6,12 +6,10 @@ def test_value_retrieval():
 
     cache.store_value("a", 1, {})
     assert 1 == cache.get_value("a", {})
-    assert 1 == cache.get_value("a")
     assert None is cache.get_value("a", {"par": 2})
 
     cache.store_value("b", 2, {"x": 1, "y": 2})
     assert 2 == cache.get_value("b", {"x": 1, "y": 2})
-    assert 2 == cache.get_value("b")
     assert None is cache.get_value("b", {})
     assert None is cache.get_value("b", {"par": 2})
     assert None is cache.get_value("b", {"x": 2, "y": 2})
@@ -22,8 +20,9 @@ def test_value_retrieval():
     assert 3 == cache.get_value("b", {"x": 1, "y": 2})
     cache.store_value("b", 4, {"x": 2, "y": 1})
     assert 4 == cache.get_value("b", {"x": 2, "y": 1})
-    assert None is cache.get_value("b", {"x": 1, "y": 2})
+    assert 3 is cache.get_value("b", {"x": 1, "y": 2})
 
     cache.empty()
-    assert None is cache.get_value("a")
-    assert None is cache.get_value("b")
+    assert None is cache.get_value("a", {})
+    assert None is cache.get_value("b", {"x": 2, "y": 1})
+    assert None is cache.get_value("b", {"x": 1, "y": 2})
