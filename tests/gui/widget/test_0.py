34b9b04f039c2295e4bb131ad8998aa0272ca4f9
Rico Belder <rico.belder@dlr.de>
2018-01-23 12:08:48 +0100

    chore(widget/test_0.py) use testing utils function no double definition

--- tests/gui/widget/test_0.py
+++ tests/gui/widget/test_0.py
@@ -2,12 +2,4 @@ import testing_utils
 
 
 def test_thread_observer_creation_list(caplog):
-    testing_utils.run_gui(gui_config={'HISTORY_ENABLED': False, 'AUTO_BACKUP_ENABLED': False})
-    try:
-        # do nothing, just open gui and close it afterwards
-        assert True
-    except:
-        raise
-    finally:
-        testing_utils.close_gui()
-        testing_utils.shutdown_environment(caplog=caplog, expected_warnings=0, expected_errors=0)
+    testing_utils.dummy_gui(caplog)
