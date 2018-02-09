55f6c0bb3f4a34eac418b61de29307f82108c456
Rico Belder <rico.belder@dlr.de>
2018-02-09 07:51:47 +0100

    chore(sm smm model): make the exception in result error and add missing set of attr

--- source/rafcon/gui/models/state_machine_manager.py
+++ source/rafcon/gui/models/state_machine_manager.py
@@ -82,8 +82,8 @@ class StateMachineManagerModel(ModelMT):
         from rafcon.gui.utils.notification_overview import NotificationOverview
         overview = NotificationOverview(info)
         if isinstance(overview['result'][-1], Exception):
-            logger.error("result type is {0} and the full notification {1}".format(type(overview['result'][-1]),
-                                                                                  overview))
+            logger.error("result type is {0} and the full notification {1}"
+                         "".format(type(overview['result'][-1]), overview))
             return
         # TODO end ######################################
         if info["method_name"] == "add_state_machine":
