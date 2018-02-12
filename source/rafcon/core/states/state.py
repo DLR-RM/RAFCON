6e081e2aa965bd139061393880dd330046509416
Rico Belder <rico.belder@dlr.de>
2018-02-12 17:50:08 +0100

    chore(prints/comments): remove acccidentally commit pints and commented code

--- source/rafcon/core/states/state.py
+++ source/rafcon/core/states/state.py
@@ -633,7 +633,6 @@ class State(Observable, YAMLObject, JSONObject, Hashable):
         :param bool force: if the removal should be forced without checking constraints
         :param bool destroy: a flag that signals that the state element will be fully removed and disassembled
         """
-        print self.__class__.__name__, "State method remove", state_element, recursive, force, destroy
         if isinstance(state_element, Outcome):
             return self.remove_outcome(state_element.outcome_id, force=force, destroy=destroy)
         elif isinstance(state_element, InputDataPort):
