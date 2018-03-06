cacd6c538b3ec10a93162acf13d68680e7e7dc66
Rico Belder <rico.belder@dlr.de>
2018-03-06 15:16:59 +0100

    fix(library-state-model): do not create state_copy logger message if not initialized

--- source/rafcon/gui/models/library_state.py
+++ source/rafcon/gui/models/library_state.py
@@ -91,7 +91,8 @@ class LibraryStateModel(AbstractStateModel):
                 self.state_copy.prepare_destruction(recursive)
                 self.state_copy = None
             else:
-                logger.verbose("Multiple calls of prepare destruction for {0}".format(self))
+                if self.state_copy_initialized:
+                    logger.verbose("Multiple calls of prepare destruction for {0}".format(self))
 
             # The next lines are commented because not needed and create problems if used why it is an open to-do
             # for port in self.input_data_ports[:] + self.output_data_ports[:] + self.outcomes[:]:
