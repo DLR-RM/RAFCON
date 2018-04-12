068709ba050678813338e7fbcc0e30aee2a5b2ad
Franz Steinmetz <franz.steinmetz@dlr.de>
2018-04-12 17:12:30 +0200

    refactor(tests): Use new aliases in tests

--- tests/gui/test_start_script.py
+++ tests/gui/test_start_script.py
@@ -76,7 +76,7 @@ def test_initial_default_config_folder_generation():
 
 
 def test_start_script_valid_config():
-    """ Test rafcon_start console call which run a rafcon instance with handed config.yaml file, open a state machine,
+    """ Test rafcon_core console call which run a rafcon instance with handed config.yaml file, open a state machine,
     run it and final checks the output file on consistency.
     """
     testing_utils.dummy_gui(None)
@@ -84,7 +84,7 @@ def test_start_script_valid_config():
     bin_path = join(dirname(testing_utils.RAFCON_PATH), "..", "bin")
     start_path = testing_utils.get_test_sm_path(join("unit_test_state_machines", "start_script_test"))
     config = join(testing_utils.TESTS_PATH, "assets", "configs", "valid_config", "config.yaml")
-    cmd = "export PATH={0}:$PATH && rafcon_start -o {1} -c {2}".format(bin_path, start_path, config)
+    cmd = "export PATH={0}:$PATH && rafcon_core -o {1} -c {2}".format(bin_path, start_path, config)
     print "\ntest_start_script_valid_config: \n", cmd
     cmd_res = subprocess.call(cmd, shell=True)
     assert cmd_res == 0
@@ -98,6 +98,7 @@ def test_start_script_valid_config():
 def test_start_script_valid_rmpm_env():
     """Tests the execution of rafcon_start in an environment created by RMPM
     """
+    # TODO: replace rafcon_start with rafcon_core after the next release
     testing_utils.dummy_gui(None)
     import distutils.spawn
     rmpm_env = os.environ.copy()
