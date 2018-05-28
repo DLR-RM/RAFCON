824fea7e07873af4f1d838cae17f417fa87378d3
Rico Belder <rico.belder@dlr.de>
2018-05-28 10:48:30 +0200

    feat(state machine editor): initially focus root state automatically
    Add the set_focus_to_root_state method which calls its self till it is focused enought
    to fill to view-port in one axis by 80%.

--- source/rafcon/gui/controllers/graphical_editor_gaphas.py
+++ source/rafcon/gui/controllers/graphical_editor_gaphas.py
@@ -739,6 +739,16 @@ class GraphicalEditorController(ExtendedController):
                 method_name = 'data_flow_change'
         return method_name, model, result, args, instance
 
+    def set_focus_to_root_state(self):
+        state_machine_m = self.model
+        state_machine_m.selection.focus = state_machine_m.root_state
+        # check_relative size in view and call it again if the state is still very small
+        state_v = self.canvas.get_view_for_model(state_machine_m.root_state)
+        state_size = self.view.editor.get_matrix_i2v(state_v).transform_distance(state_v.width, state_v.height)
+        viewport_size = self.view.editor.allocation[2], self.view.editor.allocation[3]
+        if state_size[0] < 0.8*viewport_size[0] and state_size[1] < 0.8*viewport_size[1]:
+            self.set_focus_to_root_state()
+
     def setup_canvas(self):
         logger.verbose("start setup canvas")
         start_time_view_generation = time.time()
@@ -755,6 +765,9 @@ class GraphicalEditorController(ExtendedController):
                             " when the state machine is being saved.")
         logger.verbose("Time spent in setup canvas {0} state machine {1}".format(time.time() - start_time_view_generation,
                                                                                  self.model.state_machine_id))
+
+        # finally set the focus to the root state (needs to be idle add to be executed after gaphas drawing is finished)
+        gtk.idle_add(self.set_focus_to_root_state)
 
     @lock_state_machine
     def add_state_view_for_model(self, state_m, parent_v=None, rel_pos=(0, 0), size=(100, 100), hierarchy_level=1):
