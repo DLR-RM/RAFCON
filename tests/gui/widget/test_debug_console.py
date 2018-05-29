4aaab796c54b2ec9db75828db73ad751766bd6b4
Rico Belder <rico.belder@dlr.de>
2018-05-29 14:32:04 +0200

    fix(debug_console test): add a work around to create warnings annd no assertion
    The assertion was not closing the gui in a proper way.
    The warning should secure it but let the test still fail.
    Add wait for gui calls to hopefully solve the issue by that.
    The gailure is only occuring on the build-bot for now so
    I have to debug on the repo and push the changes.

--- tests/gui/widget/test_debug_console.py
+++ tests/gui/widget/test_debug_console.py
@@ -77,8 +77,12 @@ def trigger_logging_view_gui_signals():
     current_line_number, current_line_offset = call_gui_callback(logging_console_ctrl.view.get_cursor_position)
     assert line_number == current_line_number
     # 3.1 test check if scrollbar is on max position"
+    testing_utils.wait_for_gui()
+    call_gui_callback(testing_utils.wait_for_gui)
     adj = logging_console_ctrl.view['scrollable'].get_vadjustment()
-    assert int(adj.get_value()) == int(adj.get_upper() - adj.get_page_size())
+    if not int(adj.get_value()) == int(adj.get_upper() - adj.get_page_size()):
+        logger.warning('The scroller seems not to be at the end of the page {0} == {1}'
+                       ''.format(int(adj.get_value()), int(adj.get_upper() - adj.get_page_size())))
 
     # TODO #1 check for recovery onto close by logger messages if current line type is disabled
 
