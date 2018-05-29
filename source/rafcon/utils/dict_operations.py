6a3e4ad7936c26c7d37d1f41cc91e31768e83129
Sebastian Brunner <sebastian.brunner@dlr.de>
2018-05-29 10:47:19 +0200

    feat(dict_operations): add method to check if object reference in dict
    add content

---
+++ source/rafcon/utils/dict_operations.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2015-2017 DLR
+#
+# All rights reserved. This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License v1.0 which
+# accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+# Sebastian Brunner <sebastian.brunner@dlr.de>
+
+
+def check_if_dict_contains_object_reference_in_values(object_to_check, dict_to_check):
+    """ Method to check if an object is inside the values of a dict.
+    A simple object_to_check in dict_to_check.values() does not work as it uses the __eq__ function of the object
+    and not the object reference.
+
+    :param object_to_check: The target object.
+    :param dict_to_check: The dict to search in.
+    :return:
+    """
+    for key, value in dict_to_check.iteritems():
+        if object_to_check is value:
+            return True
+    return False
