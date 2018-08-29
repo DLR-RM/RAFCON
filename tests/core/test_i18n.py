6e79d0dac44dcd87887511d9269e762095c20435
Sebastian Brunner <Sebastian.Brunner@dlr.de>
2018-08-29 15:48:29 +0200

    fix(language tests): robust against different os env vars for language
    both os env vars LANG and LANGUAGE are used to set the language of the system
    thus, the tests need to set both of them

--- tests/core/test_i18n.py
+++ tests/core/test_i18n.py
@@ -6,7 +6,8 @@ import testing_utils
 
 
 def test_invalid_locale_setting(caplog):
-    os.environ["LANG"] = "de"
+    os.environ["LANG"] = "not_existing"
+    os.environ["LANGUAGE"] = "not_existing"
     i18n.setup_l10n()
 
     testing_utils.assert_logger_warnings_and_errors(caplog=caplog, expected_warnings=1)
@@ -14,8 +15,15 @@ def test_invalid_locale_setting(caplog):
 
 def test_basic_string_translation(caplog):
     os.environ["LANG"] = "de_DE.UTF-8"
+    os.environ["LANGUAGE"] = "de_DE.UTF-8"
     i18n.setup_l10n()
 
     assert _("Remove") == "Entfernen"
 
     testing_utils.assert_logger_warnings_and_errors(caplog=caplog)
+
+
+if __name__ == '__main__':
+    test_invalid_locale_setting(None)
+    test_basic_string_translation(None)
+    # pytest.main([__file__])
--- tests/gui/test_i18n.py
+++ tests/gui/test_i18n.py
@@ -6,11 +6,12 @@ import testing_utils
 
 
 def test_gtk_translation(caplog):
+    os.environ["LANG"] = "de_DE.UTF-8"
+    os.environ["LANGUAGE"] = "de_DE.UTF-8"
+    # i18n.setup_l10n() is called inside run_gui()
     testing_utils.run_gui()
 
     try:
-        os.environ["LANG"] = "de_DE.UTF-8"
-        i18n.setup_l10n()
 
         import rafcon.gui.singleton
         main_window_controller = rafcon.gui.singleton.main_window_controller
@@ -18,8 +19,14 @@ def test_gtk_translation(caplog):
         gvm_view = gvm_controller.view
         remove_button = gvm_view["delete_global_variable_button"]
 
+        print _("Remove"), remove_button.get_label()
         assert _("Remove") == "Entfernen" == remove_button.get_label()
 
     finally:
         testing_utils.close_gui()
         testing_utils.shutdown_environment(caplog=caplog)
+
+
+if __name__ == '__main__':
+    test_gtk_translation(None)
+    # pytest.main([__file__])
