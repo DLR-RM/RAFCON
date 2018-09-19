9a84764d5cc929b0ea1d3f42b9dd1ea80ab19678
Franz Steinmetz <franz.steinmetz@dlr.de>
2018-09-19 11:23:13 +0200

    style(hierarchy_abortion_handling): Use logger instead of prints

--- share/examples/functionality_examples/hierarchy_abortion_handling/error_propagation_test_BNXYRR/faulty_hierarchy_CLRZWT/execution_abortion_handling_MWHPPQ/script.py
+++ share/examples/functionality_examples/hierarchy_abortion_handling/error_propagation_test_BNXYRR/faulty_hierarchy_CLRZWT/execution_abortion_handling_MWHPPQ/script.py
@@ -1,11 +1,11 @@
 
 
 def execute(self, inputs, outputs, gvm):
-    print "Handling execution abortion ... "
+    self.logger.info("Handling execution abortion ...")
     
     if "error" in inputs:
-        print inputs["error"]
-        
+        self.logger.error(inputs["error"])
+
     number1 = 0
     number2 = 1 / number1
     
