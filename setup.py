8f0acfeca00544c8c3309fc0658940efe3b29d44
Franz Steinmetz <franz.steinmetz@dlr.de>
2018-09-25 13:57:08 +0200

    fix(setup.py): Add missing copyright text

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python2.7
 
-# Copyright
+# Copyright (C) 2015-2018 DLR
+#
+# All rights reserved. This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License v1.0 which
+# accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+# Franz Steinmetz <franz.steinmetz@dlr.de>
+# Rico Belder <rico.belder@dlr.de>
+# Sebastian Brunner <sebastian.brunner@dlr.de>
 
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
