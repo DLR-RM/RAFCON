0dbf5b6410956236056081917b30103f298e82a0
Franz Steinmetz <franz.steinmetz@dlr.de>
2018-09-25 13:58:38 +0200

    fix(execution_log_viewer.py): Add missing copyright text

--- source/rafcon/gui/controllers/execution_log_viewer.py
+++ source/rafcon/gui/controllers/execution_log_viewer.py
@@ -1,4 +1,12 @@
-# Copyright
+# Copyright (C) 2015-2018 DLR
+#
+# All rights reserved. This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License v1.0 which
+# accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+# Rico Belder <rico.belder@dlr.de>
 
 # example basictreeview.py
 
