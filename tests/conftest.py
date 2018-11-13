1de0ed6f6c119c76af5132a68a383e5ae9aec034
Franz Steinmetz <franz.steinmetz@dlr.de>
2018-11-13 14:56:39 +0100

    test(confest): Restore all configs before all tests and after finishing
    Before running the tests, all configurations are stored. Before each
    test and after finishing the test suite, the configs are restored. This
    should prevent side effects between tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+import os
+
 # Unfortunately this approach does not work to make sure to initialize the gui singletons from a gui thread
 # Problem: after executing conftest.py all modules are re-imported
 # and thus all variables (incl. singletons) are reinitialized
@@ -16,9 +18,45 @@ from __future__ import absolute_import
 #             testing_utils.close_gui()
 #             testing_utils.shutdown_environment()
 
+configs = [("core", "config.yaml"), ("gui", "gui_config.yaml"), ("runtime", "runtime_config.yaml")]
+config_contents = {}
+
+
+def pytest_configure(config):
+    store_configs()
+
 
 def pytest_unconfigure(config):
+    restore_configs()
     clean_temp_test_directory()
+
+
+def pytest_runtest_setup(item):
+    restore_configs()
+
+
+def store_configs():
+    global configs
+    config_path = os.path.join(os.path.expanduser('~'), '.config', 'rafcon')
+
+    for config_name, file_name in configs:
+        try:
+            with open(os.path.join(config_path, file_name), 'r') as config_file:
+                config_contents[config_name] = config_file.read()
+        except IOError:
+            pass
+
+
+def restore_configs():
+    config_path = os.path.join(os.path.expanduser('~'), '.config', 'rafcon')
+
+    for config_name, file_name in configs:
+        if config_name in config_contents:
+            try:
+                with open(os.path.join(config_path, file_name), 'w') as config_file:
+                    config_file.write(config_contents[config_name])
+            except IOError:
+                pass
 
 
 def clean_temp_test_directory():
