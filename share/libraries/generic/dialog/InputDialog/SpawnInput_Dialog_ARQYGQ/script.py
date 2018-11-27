3c6fb2052cff44b31479a010f1ec0699b34ff67c
Rico Belder <rico.belder@dlr.de>
2018-11-27 19:00:23 +0100

    fix(generic libraries): fix library that use RafconInputDialog

--- share/libraries/generic/dialog/InputDialog/SpawnInput_Dialog_ARQYGQ/script.py
+++ share/libraries/generic/dialog/InputDialog/SpawnInput_Dialog_ARQYGQ/script.py
@@ -1,3 +1,4 @@
+from gi.repository import GObject
 from rafcon.gui.utils.dialog import RAFCONInputDialog
 
 def execute(self, inputs, outputs, gvm):
@@ -7,17 +8,37 @@ def execute(self, inputs, outputs, gvm):
         self.logger.error("Please specify exactly two buttons as a list")
         return "aborted"
         
-    dialog_window = RAFCONInputDialog(markup_text=inputs['message_text'],
-                                      button_texts=inputs['buttons'],
-                                      checkbox_text=inputs['checkbox_text'])
+    def run_dialog(event, result, logger):
+        dialog_window = RAFCONInputDialog(markup_text=inputs['message_text'],
+                                  button_texts=inputs['buttons'],
+                                  checkbox_text=inputs['checkbox_text'])
+        response_id = dialog_window.run()
+    
+        outputs['entered_text'] = dialog_window.get_entry_text()
+        outputs['checkbox_state'] = dialog_window.get_checkbox_state()
+        result.append(response_id)
+        result.append(dialog_window)
+
+        event.set()
+    
+    event = self._preempted
+    result = []
+    GObject.idle_add(run_dialog, event, result, self.logger)    
+    
+    # Event is either set by the dialog or by an external preemption request
+    event.wait()
+    
+    response_id = result[0]
+    dialog  = result[1]
+    
+    # The dialog was not closed by the user, but we got a preemption request
+    dialog.destroy()
+    if response_id is None:
+        return "preempted"
+        
+    event.clear()
+        
     abort = inputs['abort_on_quit']
-    
-    response_id = dialog_window.run()
-    
-    outputs['entered_text'] = dialog_window.get_entry_text()
-    outputs['checkbox_state'] = dialog_window.get_checkbox_state()
-    
-    dialog_window.destroy()
     
     if response_id == 1:
         return 0
