c39cf920117f695285287c12a5fa73d3fcfa11f6
Sebastian Brunner <Sebastian.Brunner@dlr.de>
2019-02-18 10:50:16 +0100

    fix(dynamic_state_insertion): create state with call_gui_callback

--- tests/assets/unit_test_state_machines/dynamic_library_insertion/dynamic_library_insertion_PXDJJT/insert_library_state_BXGHOZ/script.py
+++ tests/assets/unit_test_state_machines/dynamic_library_insertion/dynamic_library_insertion_PXDJJT/insert_library_state_BXGHOZ/script.py
@@ -1,5 +1,6 @@
 from rafcon.core.states.library_state import LibraryState
 from rafcon.gui.utils import wait_for_gui
+from rafcon.utils.gui_functions import call_gui_callback
 import time
 
 def execute(self, inputs, outputs, gvm):
@@ -8,16 +9,15 @@ def execute(self, inputs, outputs, gvm):
     #libary_name = "bake_library1"
     libary_path = "unit_test_state_machines/dynamic_library_insertion_libraries"
     libary_name = "test_library"
-    e = LibraryState(libary_path, libary_name, name="test_library_state")
-    self.parent.add_state(e)
-    self.parent.add_transition(self.state_id, 0, e.state_id, None)
-    wait_for_gui()
-    self.parent.add_transition(e.state_id, 0, self.parent.state_id, 0)
+    e = call_gui_callback(LibraryState, libary_path, libary_name, name="test_library_state")
+    call_gui_callback(self.parent.add_state, e)
+    call_gui_callback(self.parent.add_transition, self.state_id, 0, e.state_id, None)
+    call_gui_callback(self.parent.add_transition, e.state_id, 0, self.parent.state_id, 0)
     wait_for_gui()
     parent_id = None
     for d_id, d in self.parent.output_data_ports.items():
         if d.name == "output_0":
             parent_id = d_id
-    self.parent.add_data_flow(e.state_id, 2, self.parent.state_id, parent_id)
+    call_gui_callback(self.parent.add_data_flow, e.state_id, 2, self.parent.state_id, parent_id)
     wait_for_gui()
     return 0
