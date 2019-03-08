735c8ead1f70938ee82727356e7f113175be6083
Franz Steinmetz <franz.steinmetz@dlr.de>
2019-03-08 11:18:21 +0100

    fix(gui.init): Handle ImportError of gi

--- source/rafcon/gui/__init__.py
+++ source/rafcon/gui/__init__.py
@@ -10,7 +10,11 @@
 # Sebastian Brunner <sebastian.brunner@dlr.de>
 
 import rafcon.gui.interface
-import gi
-gi.require_version('Gtk', '3.0')
-gi.require_version('PangoCairo', '1.0')
-gi.require_version('GtkSource', '3.0')
+
+try:
+    import gi
+    gi.require_version('Gtk', '3.0')
+    gi.require_version('PangoCairo', '1.0')
+    gi.require_version('GtkSource', '3.0')
+except ImportError:
+    pass
