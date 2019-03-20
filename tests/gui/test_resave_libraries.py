f6c1881395251bf742378ebd4cd32330a1ab8556
Franz Steinmetz <franz.steinmetz@dlr.de>
2019-03-20 09:43:30 +0100

    test(resave_*): Specify RAFCON_TEMP_PATH_CONFIGS as gui_config_path

--- tests/gui/test_resave_libraries.py
+++ tests/gui/test_resave_libraries.py
@@ -11,10 +11,11 @@ def test_resave(caplog):
     folder_to_convert = testing_utils.TUTORIAL_PATH
     target_folder = os.path.join(testing_utils.RAFCON_TEMP_PATH_TEST_BASE, "resave_test")
     config_path = os.path.join(testing_utils.TESTS_PATH, "assets", "configs", "valid_config")
+    gui_config_path = testing_utils.RAFCON_TEMP_PATH_CONFIGS
     print("folder to convert: " + folder_to_convert)
     print("config path: " + config_path)
     import rafcon.gui.resave_state_machines as resave
-    resave.convert_libraries_in_path(config_path, folder_to_convert, target_folder)
+    resave.convert_libraries_in_path(config_path, folder_to_convert, target_folder, gui_config_path)
 
     testing_utils.shutdown_environment(caplog=caplog, unpatch_threading=False)
 
