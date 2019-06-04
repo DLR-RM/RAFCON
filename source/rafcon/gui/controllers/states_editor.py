8e2c78b73a2b00aac49c1055df2bcdf153ea5133
Sebastian Brunner <Sebastian.Brunner@dlr.de>
2019-06-04 13:53:39 +0200

    fix(states editor): fix destroy function
    correct order of destroy function calls

--- source/rafcon/gui/controllers/states_editor.py
+++ source/rafcon/gui/controllers/states_editor.py
@@ -379,7 +379,7 @@ class StatesEditorController(ExtendedController):
         if old_is_dirty is not tab_list[state_identifier]['source_code_view_is_dirty']:
             self.update_tab_label(source_script_state_m)
 
-    def destroy_page(self, tab_dict):
+    def _destroy_page(self, tab_dict):
         """ Destroys desired page
 
         Disconnects the page from signals and removes interconnection to parent-controller or observables.
@@ -406,20 +406,20 @@ class StatesEditorController(ExtendedController):
         """
         # delete old controller references
         if delete and state_identifier in self.closed_tabs:
-            self.destroy_page(self.closed_tabs[state_identifier])
+            self._destroy_page(self.closed_tabs[state_identifier])
             del self.closed_tabs[state_identifier]
 
         # check for open page of state
         if state_identifier in self.tabs:
             page_to_close = self.tabs[state_identifier]['page']
             current_page_id = self.view.notebook.page_num(page_to_close)
+            self.view.notebook.remove_page(current_page_id)
             if not delete:
                 self.closed_tabs[state_identifier] = self.tabs[state_identifier]
             else:
-                self.destroy_page(self.tabs[state_identifier])
-            del self.tabs[state_identifier]
-            # Finally remove the page, this triggers the callback handles on_switch_page
-            self.view.notebook.remove_page(current_page_id)
+                self._destroy_page(self.tabs[state_identifier])
+            if state_identifier in self.tabs:
+                del self.tabs[state_identifier]
 
     def find_page_of_state_m(self, state_m):
         """Return the identifier and page of a given state model
