a756b2e5d53fd23faa3a93e0c7797d85076eb6a6
Franz Steinmetz <franz.steinmetz@dlr.de>
2019-07-12 14:59:27 +0200

    refactor(installation): Refactoring, as no longer called from setup

--- source/rafcon/utils/installation.py
+++ source/rafcon/utils/installation.py
@@ -10,20 +10,15 @@
 # Sebastian Brunner <sebastian.brunner@dlr.de>
 
 import os
-from os import path
+from os.path import join, dirname, abspath, isfile, isdir, splitext, expanduser
 import sys
-from imp import load_source
 import shutil
 import subprocess
-import distutils.log
 from distutils.dir_util import copy_tree
 
-try:
-    from rafcon.utils import resources
-except ImportError:
-    script_path = path.realpath(__file__)
-    resources_script_path = path.join(path.dirname(script_path), "resources.py")
-    resources = load_source("resources", resources_script_path)
+from rafcon.utils import resources, log
+
+logger = log.get_logger(__name__)
 
 
 def started_without_installation():
@@ -32,17 +27,14 @@ def started_without_installation():
     if isfile(pt_file_path):
         return True
 
-def install_fonts(logger=None, restart=False):
-    if logger:
-        log = logger
-    else:
-        log = distutils.log
+
+def install_fonts(restart=False):
     try:
         import gi
         gi.require_version('Gtk', '3.0')
         from gi.repository import Gtk
     except (ImportError, ValueError):
-        log.warn("No GTK found. Will not install fonts.")
+        logger.warn("No GTK found. Will not install fonts.")
         return
 
     font_names_to_be_installed = ["Source Sans Pro", "FontAwesome"]
@@ -51,24 +43,23 @@ def install_fonts(logger=None, restart=False):
         tv = Gtk.TextView()
         context = tv.get_pango_context()
     except Exception as e:
-        log.error("Could not get pango context. Will not install fonts: {}".format(e))
+        logger.error("Could not get pango context. Will not install fonts: {}".format(e))
         return
     if not context:  # A Pango context is not always available
-        log.warn("Could not get pango context. Will not install fonts.")
+        logger.warn("Could not get pango context. Will not install fonts.")
         return
     existing_fonts = context.list_families()
     existing_font_faces = {font.get_name(): [face.get_face_name() for face in font.list_faces()]
                            for font in existing_fonts
                            if font.get_name() in font_names_to_be_installed}
 
-    user_otf_fonts_folder = os.path.join(os.path.expanduser('~'), '.fonts')
+    user_otf_fonts_folder = join(expanduser('~'), '.fonts')
 
     font_installed = False
     try:
         for font_name in font_names_to_be_installed:
             # A font is a folder one or more font faces
             fonts_folder = resources.get_data_file_path("rafcon", "gui", "fonts", font_name)
-            # fonts_folder = os.path.join(assets_path, "fonts", font_name)
             num_faces_to_be_installed = len([name for name in os.listdir(fonts_folder) if name.endswith(".otf")])
             num_faces_installed = 0
             # default case: font is not installed yet!
@@ -76,32 +67,31 @@ def install_fonts(logger=None, restart=False):
                 num_faces_installed = len(existing_font_faces[font_name])
 
             if num_faces_to_be_installed <= num_faces_installed:
-                log.debug("Font '{0}' already installed".format(font_name))
+                logger.debug("Font '{0}' already installed".format(font_name))
                 continue
 
             specific_user_otf_fonts_folder = user_otf_fonts_folder
             if num_faces_to_be_installed > 1:
-                specific_user_otf_fonts_folder = os.path.join(user_otf_fonts_folder, font_name)
+                specific_user_otf_fonts_folder = join(user_otf_fonts_folder, font_name)
 
-            log.info("Installing font '{0}' to {1}".format(font_name, specific_user_otf_fonts_folder))
-            if not os.path.isdir(specific_user_otf_fonts_folder):
+            logger.info("Installing font '{0}' to {1}".format(font_name, specific_user_otf_fonts_folder))
+            if not isdir(specific_user_otf_fonts_folder):
                 os.makedirs(specific_user_otf_fonts_folder)
             for font_face in os.listdir(fonts_folder):
-                target_font_file = os.path.join(specific_user_otf_fonts_folder, font_face)
-                source_font_file = os.path.join(fonts_folder, font_face)
+                target_font_file = join(specific_user_otf_fonts_folder, font_face)
+                source_font_file = join(fonts_folder, font_face)
                 shutil.copy(source_font_file, target_font_file)
             font_installed = True
     except IOError as e:
-        log.error("Could not install fonts, IOError: {}".format(e))
+        logger.error("Could not install fonts, IOError: {}".format(e))
         return
 
     if font_installed:
-        log.info("Running font detection ...")
+        logger.info("Running font detection ...")
         fail = subprocess.call(['fc-cache', '-fv', user_otf_fonts_folder])
         if fail:
-            log.warn("Could not run font detection. RAFCON might not find the correct fonts.")
+            logger.warn("Could not run font detection. RAFCON might not find the correct fonts.")
         if restart:
-            log.info("Restarting RAFCON to apply new fonts...")
             python = sys.executable
             environ = dict(**os.environ)
             # Passing this to the new RAFCON environment will prevent further checks and thus restarts
@@ -112,8 +102,10 @@ def install_fonts(logger=None, restart=False):
 
 
 def install_locally_required_files():
+    source_share_folder = resources.get_repository_share_path()
+    if not source_share_folder:
+        logger.warn("Cannot find repository required for installation of icons and gtksourceview styles")
     local_user_data_folder = resources.installation_share_folder
-    source_share_folder = resources.get_relative_share_path()
 
     for folder in ["gtksourceview-3.0", "icons"]:
         try:
@@ -124,23 +116,20 @@ def install_locally_required_files():
 
 
 def create_mo_files():
-    data_files = []
     domain = "rafcon"
     assert "setup.py" in os.listdir(os.curdir)
-    rel_localedir = path.join('share', 'rafcon', 'locale')
-    localedir = os.path.join(os.curdir, rel_localedir)
+    rel_localedir = join('share', 'rafcon', 'locale')
+    localedir = join(os.curdir, rel_localedir)
     # Assert that we are in the root directory of the RAFCON repository
     po_files = [po_file
                 for po_file in next(os.walk(localedir))[2]
-                if path.splitext(po_file)[1] == '.po']
+                if splitext(po_file)[1] == '.po']
     for po_file in po_files:
-        po_path = path.join(localedir, po_file)
-        lang, extension = path.splitext(po_file)
+        po_path = join(localedir, po_file)
+        lang, extension = splitext(po_file)
         mo_file = domain + '.mo'
-        mo_dir = path.join(localedir, lang, 'LC_MESSAGES')
-        mo_path = path.join(mo_dir, mo_file)
-        mo_dir_rel = path.join(rel_localedir, lang, 'LC_MESSAGES')
-        mo_path_rel = path.join(mo_dir_rel, mo_file)
+        mo_dir = join(localedir, lang, 'LC_MESSAGES')
+        mo_path = join(mo_dir, mo_file)
         try:
             os.makedirs(mo_dir)
         except os.error:  # already exists
@@ -148,5 +137,5 @@ def create_mo_files():
         msgfmt_cmd = 'msgfmt -o {} {}'.format(mo_path, po_path)
         result = subprocess.call(msgfmt_cmd, shell=True)
         if result != 0:  # Compilation successful
-            distutils.log.warn("Could not compile translation '{}'. RAFCON will not be available in this "
-                               "language.".format(lang))
+            logger.warn("Could not compile translation '{}'. RAFCON will not be available in this "
+                         "language.".format(lang))
