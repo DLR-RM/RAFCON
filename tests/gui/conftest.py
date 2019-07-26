eccd095735f719b0a1714f75af300326027dbc48
Franz Steinmetz <franz.steinmetz@dlr.de>
2019-07-26 14:49:43 +0200

    test(gui/conftest): extend gui fixture
    * add function to restart GUI
    * decide whether to run with or without GUI

--- tests/gui/conftest.py
+++ tests/gui/conftest.py
@@ -1,16 +1,18 @@
 import pytest
 
-from tests import utils
+from copy import deepcopy
 
-from rafcon.utils.gui_functions import call_gui_callback
+from tests import utils
 
 
 class GUITester(object):
 
-    def __init__(self):
+    def __init__(self, with_gui=True, config=None):
         import rafcon.core.singleton as core_singletons
         import rafcon.gui.singleton as gui_singletons
 
+        self.with_gui = with_gui
+        self.config = config or {}
         self.post_test = None
 
         self.expected_warnings = 0
@@ -19,26 +21,46 @@ class GUITester(object):
         self.singletons = gui_singletons
         self.core_singletons = core_singletons
 
+    def restart(self, force_quit=False):
+        utils.close_gui(force_quit=force_quit)
+        utils.shutdown_environment()
+        utils.run_gui(**deepcopy(self.config))
+
     def __call__(self, *args, **kwargs):
-        return call_gui_callback(*args, **kwargs)
+        if self.with_gui:
+            from rafcon.utils.gui_functions import call_gui_callback
+            return call_gui_callback(*args, **kwargs)
+        else:
+            from rafcon.gui.utils import wait_for_gui
+            func = args[0]
+            result = func(*args[1:], **kwargs)
+            wait_for_gui()
+            return result
 
 
 
 @pytest.fixture
 def gui(request, caplog):
-    utils.dummy_gui(caplog)
-
     parameters = {} if not hasattr(request, "param") else request.param
-    utils.run_gui(core_config=parameters.get("core_config"),
-                  gui_config=parameters.get("gui_config"),
-                  runtime_config=parameters.get("runtime_config"),
-                  libraries=parameters.get("libraries"))
+    with_gui = parameters.get("with_gui", True)
 
-    gui_tester = GUITester()
+    config = {parameter_name: parameters.get(parameter_name) for parameter_name in
+              ["core_config", "gui_config", "runtime_config", "libraries"]}
+
+    utils.dummy_gui(caplog)
+    if with_gui:
+        utils.run_gui(**deepcopy(config))
+    else:
+        utils.initialize_environment(gui_already_started=False, **deepcopy(config))
+
+    gui_tester = GUITester(with_gui, config)
     yield gui_tester
 
-    utils.close_gui()
-    utils.shutdown_environment(caplog=caplog, expected_warnings=gui_tester.expected_warnings, expected_errors=gui_tester.expected_errors)
+    if with_gui:
+        utils.close_gui()
+    utils.shutdown_environment(caplog=caplog, unpatch_threading=with_gui,
+                               expected_warnings=gui_tester.expected_warnings,
+                               expected_errors=gui_tester.expected_errors)
 
     gui_tester.post_test and gui_tester.post_test()
 
