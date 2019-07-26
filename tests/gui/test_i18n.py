9206b55ffd4d4e98c6f889e147982d4f0f45965f
Franz Steinmetz <franz.steinmetz@dlr.de>
2019-07-26 14:53:38 +0200

    refactor(test): use gui fixture for remaining tests
    There is now no more run_gui in any test! All make use of the gui
    fixture.

--- tests/gui/test_i18n.py
+++ tests/gui/test_i18n.py
@@ -1,28 +1,20 @@
 from __future__ import print_function
-from tests import utils as testing_utils
 from tests.core.test_i18n import use_locale, create_mo_files
 
 
-def test_gtk_translation(caplog, monkeypatch):
+def test_gtk_translation(gui, monkeypatch):
     create_mo_files()
+
     with use_locale("de_DE.UTF-8", monkeypatch):
-        # i18n.setup_l10n() is called inside run_gui()
-        testing_utils.run_gui()
+        gui.restart()
 
-        try:
+        main_window_controller = gui.singletons.main_window_controller
+        gvm_controller = main_window_controller.get_controller('global_variable_manager_ctrl')
+        gvm_view = gvm_controller.view
+        remove_button = gvm_view["delete_global_variable_button"]
 
-            import rafcon.gui.singleton
-            main_window_controller = rafcon.gui.singleton.main_window_controller
-            gvm_controller = main_window_controller.get_controller('global_variable_manager_ctrl')
-            gvm_view = gvm_controller.view
-            remove_button = gvm_view["delete_global_variable_button"]
-
-            print("Found text in label is: ", _("Remove"), remove_button.get_label())
-            assert _("Remove") == "Entfernen" == remove_button.get_label()
-
-        finally:
-            testing_utils.close_gui()
-            testing_utils.shutdown_environment(caplog=caplog)
+        print("Found text in label is: ", _("Remove"), remove_button.get_label())
+        assert _("Remove") == "Entfernen" == remove_button.get_label()
 
 
 if __name__ == '__main__':
