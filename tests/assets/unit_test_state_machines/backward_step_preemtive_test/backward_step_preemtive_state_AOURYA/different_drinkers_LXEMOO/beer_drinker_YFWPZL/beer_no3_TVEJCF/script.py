c51a6db21b5b65093a7c1511c7b5a16bb4b981a2
Sebastian Brunner <Sebastian.Brunner@dlr.de>
2019-11-22 13:00:58 +0100

    fix(backward_preemptive_test): synchronize two parallel branches
    this is a crazy race conditions: without this fix, on triggering one step:
    - concurrent container 1 could execute its next state (the last of its child states), finish the state's execution, tell its parent (i.e the preemptive container) that it has finished and preempt child2
    - before child2 was able to even start its next state

--- tests/assets/unit_test_state_machines/backward_step_preemtive_test/backward_step_preemtive_state_AOURYA/different_drinkers_LXEMOO/beer_drinker_YFWPZL/beer_no3_TVEJCF/script.py
+++ tests/assets/unit_test_state_machines/backward_step_preemtive_test/backward_step_preemtive_state_AOURYA/different_drinkers_LXEMOO/beer_drinker_YFWPZL/beer_no3_TVEJCF/script.py
@@ -1,6 +1,14 @@
+import time
 
 def execute(self, inputs, outputs, gvm):
     gvm.set_variable("beers", 3)
+    while True:
+        time.sleep(0.010)
+        whiskey = gvm.get_variable('whiskey')
+        if not whiskey:  # whiskey no.1 not executed yet!
+            continue
+        if whiskey == 3:  # whiskey no.3 was started
+            break
     return 0
     
 def backward_execute(self, inputs, outputs, gvm):
