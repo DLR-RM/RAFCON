f8c95845c0ebf1c992c3f112dfa7fad5dd7bc5b3
Sebastian <sebastiangeorgbrunner@googlemail.com>
2021-06-08 17:43:42 +0200

    test: add pandas to test requirements in order to include more tests
    * test_execution_history_disabled.py
    * test_execution_history_file_log.py

--- setup.py
+++ setup.py
@@ -71,12 +71,12 @@ if sys.version_info[0] < 3:  # python2
     test_requirements = ['pytest>=3.5,<5', 'pytest-timeout', 'pytest-mock>=1.9.0,<3', 'pytest-faulthandler~=1.6.0',
                          'importlib-metadata~=2.0.0', 'zipp~=1.0.0', 'pyparsing~=2.4.0', 'mock~=3.0.0',
                          'isort~=4.3.0',
-                         'graphviz', 'pyuserinput']
+                         'graphviz', 'pyuserinput', 'pandas']
 else:  # python3
     global_requirements = ['pylint>=1.6,<2', 'psutil', 'jsonconversion~=0.2.12', 'yaml_configuration~=0.1',
                            'python-gtkmvc3-dlr~=1.0.0', 'gaphas~=1.0.0', 'future>=0.16,<0.18.0']
     test_requirements = ['pytest>=3.5,<5', 'pytest-timeout', 'pytest-mock>=1.9.0,<3', 'pytest-faulthandler~=1.6.0',
-                         'graphviz', 'pyuserinput']
+                         'graphviz', 'pyuserinput', 'pandas']
 
 test_requirements += global_requirements
 
