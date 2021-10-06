43eba9dab91cdaed257fd6b81d8ce8072c40f1cb
Vahid Mohammadi Gahrooei <vahid.mohammadi@agile-robots.com>
2021-10-06 10:31:01 +0200

    feat: add doc to the functions and make the codes more readable

--- source/rafcon/core/state_machine_manager.py
+++ source/rafcon/core/state_machine_manager.py
@@ -125,10 +125,14 @@ class StateMachineManager(Observable):
         removed_state_machine.destroy_execution_histories()
         return removed_state_machine
 
-    def remove_state_machine_by_path(self, file_system_path):
+    def remove_state_machine_by_path(self, state_machine_path):
+        """Remove the state machine for a specified state machine path from the list of registered state machines.
+
+        :param state_machine_path: the path of the state machine to be removed
+        """
         state_machine_ids = []
         for state_machine_id, state_machine in self._state_machines.items():
-            if state_machine.file_system_path == file_system_path:
+            if state_machine.file_system_path == state_machine_path:
                 state_machine_ids.append(state_machine_id)
         for state_machine_id in state_machine_ids:
             self.remove_state_machine(state_machine_id)
