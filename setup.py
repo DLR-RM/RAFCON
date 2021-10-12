604f14d904256c73f7e1f14f9173c04c171008b6
Sebastian Brunner <sebastiangeorgbrunner@hotmail.de>
2021-10-12 13:17:42 +0200

    ci: use pytest-timeout<2 as it requires a pytest>5

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ if sys.version_info[0] < 3:  # python2
 else:  # python3
     global_requirements = ['pylint>=1.6,<2', 'psutil', 'jsonconversion~=0.2.12', 'yaml_configuration~=0.1',
                            'python-gtkmvc3-dlr~=1.0.0', 'gaphas~=1.0.0', 'future>=0.16,<0.18.0']
-    test_requirements = ['pytest>=3.5,<5', 'pytest-timeout >= 2.0.1', 'pytest-mock>=1.9.0,<3', 'pytest-faulthandler~=1.6.0',
+    test_requirements = ['pytest>=3.5,<5', 'pytest-timeout<2', 'pytest-mock>=1.9.0,<3', 'pytest-faulthandler~=1.6.0',
                          'graphviz==0.16', 'pyuserinput', 'pandas~=1.1.5', 'numpy~=1.19.5']
     setup_requirements = ['pytest-runner', 'libsass >= 0.15.0']
 
