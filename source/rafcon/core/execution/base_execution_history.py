c32e30d6d875861954f853f664d8d14d08e06a3e
Sebastian <sebastiangeorgbrunner@googlemail.com>
2021-10-20 22:44:10 +0200

    feat: first draft of new execution history layout

---
+++ source/rafcon/core/execution/base_execution_history.py
@@ -0,0 +1,83 @@
+from rafcon.core.execution.execution_history_items import CallItem, ReturnItem, ConcurrencyItem, \
+    StateMachineStartItem, HistoryItem, ScopedDataItem
+from rafcon.core.execution.consumer_manager import ExecutionHistoryConsumerManager
+
+from rafcon.utils import log
+logger = log.get_logger(__name__)
+
+
+class BaseExecutionHistory(object):
+
+    def __init__(self, initial_prev=None, root_state_name="", consumer_manager=None):
+        if not consumer_manager:
+            self.consumer_manager = ExecutionHistoryConsumerManager(root_state_name)
+        else:
+            self.consumer_manager = consumer_manager
+        self.initial_prev = initial_prev
+        # saves the last history item in this variable in order to be able to get the pervious item id
+        self.last_history_item = None
+
+    def destroy(self):
+        self.initial_prev = None
+        self.last_history_item = None
+        self.consumer_manager.stop_consumers()
+
+    def shutdown(self):
+        # we don't want to keep anyting of the last run in memory, thus we can simply destroy the execution history
+        self.destroy()
+
+    def get_last_history_item(self):
+        """Returns the history item that was added last
+
+        :return: History item added last
+        :rtype: rafcon.core.execution.execution_history_items.HistoryItem
+        """
+        return self.last_history_item
+
+    def _link_item(self, current_item):
+        last_history_item = self.get_last_history_item()
+        if last_history_item is None:
+            current_item.prev = self.initial_prev
+        if last_history_item is not None:
+            current_item.prev = last_history_item
+            last_history_item.next = current_item
+            # TODO: delete the function from INEH?
+            self.last_history_item = current_item
+
+    def feed_consumers(self, execution_history_item):
+        self.consumer_manager.add_history_item_to_queue(execution_history_item)
+
+    def push_call_history_item(self, state, call_type, state_for_scoped_data, input_data=None,
+                               link_and_feed_item_to_consumers=True):
+        from rafcon.core.states.library_state import LibraryState  # delayed imported on purpose
+        if isinstance(state_for_scoped_data, LibraryState):
+            state_for_scoped_data = state_for_scoped_data.state_copy
+        history_item = CallItem(state, call_type, state_for_scoped_data, input_data, state.run_id)
+        if link_and_feed_item_to_consumers:
+            self._link_item(history_item)
+            self.feed_consumers(history_item)
+        return history_item
+
+    def push_return_history_item(self, state, call_type, state_for_scoped_data, output_data=None,
+                                 feed_item_to_consumers=True):
+        from rafcon.core.states.library_state import LibraryState  # delayed imported on purpose
+        if isinstance(state_for_scoped_data, LibraryState):
+            state_for_scoped_data = state_for_scoped_data.state_copy
+        history_item = ReturnItem(state, call_type, state_for_scoped_data, output_data,
+                                  state.run_id)
+        if feed_item_to_consumers:
+            self.feed_consumers(history_item)
+        return history_item
+
+    def push_concurrency_history_item(self, state, number_concurrent_threads, feed_item_to_consumers=True):
+        history_item = ConcurrencyItem(state, number_concurrent_threads, state.run_id, self.consumer_manager)
+        if feed_item_to_consumers:
+            self.feed_consumers(history_item)
+        return history_item
+
+    def push_state_machine_start_history_item(self, state_machine, run_id, feed_item_to_consumers=True):
+        history_item = StateMachineStartItem(state_machine, run_id)
+        if feed_item_to_consumers:
+            self.feed_consumers(history_item)
+        return history_item
+
---
+++ source/rafcon/core/execution/consumer_manager.py
@@ -0,0 +1,63 @@
+import threading
+from queue import Queue
+
+from rafcon.core.config import global_config
+from rafcon.core.execution.file_system_consumer import FileSystemConsumer
+
+from rafcon.utils import log
+logger = log.get_logger(__name__)
+
+FILE_SYSTEM_CONSUMER_NAME = "file_system_consumer"
+
+
+class ExecutionHistoryConsumerManager(object):
+
+    def __init__(self, root_state_name):
+        self.consumers = dict()
+        # Queue with infinite space
+        self.execution_history_item_queue = Queue()
+        self.interrupt = False
+        self._file_system_consumer_exists = False
+        if global_config.get_config_value("EXECUTION_LOG_ENABLE", False):
+            self.register_consumer(FILE_SYSTEM_CONSUMER_NAME, FileSystemConsumer(root_state_name))
+            self._file_system_consumer_exists = True
+        # Only have one thread here that will call the notify function of each consumer
+        # The advantage it that the consumer authors don't have to care about threading
+        # and don't have to care about when an item is popped from the queue
+        self.worker_thread = threading.Thread(target=self._feed_consumers)
+        self.worker_thread.start()
+
+    @property
+    def file_system_consumer_exists(self):
+        return self._file_system_consumer_exists
+
+    def get_file_system_consumer_file_name(self):
+        if FILE_SYSTEM_CONSUMER_NAME in self.consumers.keys():
+            return self.consumers[FILE_SYSTEM_CONSUMER_NAME].filename
+
+    def stop_consumers(self):
+        self.stop_worker_thread()
+        for consumer in self.consumers.values():
+            self.unregister_consumer(consumer)
+
+    def stop_worker_thread(self):
+        self.interrupt = True
+
+    def register_consumer(self, consumer_name, consumer):
+        self.consumers[consumer_name] = consumer
+        consumer.register()
+
+    def add_history_item_to_queue(self, execution_history_item):
+        self.execution_history_item_queue.put(execution_history_item)
+
+    def _feed_consumers(self):
+        while not self.interrupt:
+            next_execution_history_event = self.execution_history_item_queue.get(block=True)
+            self._notifyConsumers(next_execution_history_event)
+
+    def _notifyConsumers(self, execution_history_event):
+        for client in self.consumers.values():
+            client.consume(execution_history_event)
+
+    def unregister_consumer(self, consumer):
+        consumer.unregister()
