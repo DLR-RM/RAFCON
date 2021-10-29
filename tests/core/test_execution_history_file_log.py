3f3c56824089da88ee0afc5e164a10ae6dbf5848
Vahid Mohammadi Gahrooei <vahid.mohammadi@agile-robots.com>
2021-10-29 10:22:46 +0200

    feat: add new test case

--- tests/core/test_execution_history_file_log.py
+++ tests/core/test_execution_history_file_log.py
@@ -150,8 +150,42 @@ def test_execution_log_without_memory(caplog):
     finally:
         testing_utils.shutdown_environment_only_core(caplog=caplog, expected_warnings=0, expected_errors=0)
 
+def test_execution_log_without_file_system(caplog):
+    try:
+        testing_utils.initialize_environment_core(
+            core_config={
+                'IN_MEMORY_EXECUTION_HISTORY_ENABLE': True,
+                'FILE_SYSTEM_EXECUTION_HISTORY_ENABLE': False,
+                'EXECUTION_LOG_PATH': testing_utils.get_unique_temp_path()+'/test_execution_log'}
+        )
+
+        state_machine = global_storage.load_state_machine_from_path(
+            testing_utils.get_test_sm_path(os.path.join("unit_test_state_machines",
+                                                        "execution_file_log_test")))
+
+        rafcon.core.singleton.state_machine_manager.add_state_machine(state_machine)
+        rafcon.core.singleton.state_machine_execution_engine.start(state_machine.state_machine_id)
+        while not state_machine.root_state.final_outcome:
+            time.sleep(0.1)
+        rafcon.core.singleton.state_machine_execution_engine.join()
+
+        execution_history = state_machine.execution_histories[0]
+
+        assert len(execution_history) == 32
+        assert isinstance(execution_history[0], StateMachineStartItem)
+        assert isinstance(execution_history[1], CallItem)
+        assert isinstance(execution_history[2], CallItem)
+        assert isinstance(execution_history[3], ReturnItem)
+
+        rafcon.core.singleton.state_machine_manager.remove_state_machine(state_machine.state_machine_id)
+    except ImportError:  # if pandas is not installed
+        print("test_execution_log skipped as pandas is not installed")
+    finally:
+        testing_utils.shutdown_environment_only_core(caplog=caplog, expected_warnings=0, expected_errors=0)
+
 
 if __name__ == '__main__':
     test_execution_log(None)
     test_execution_log_without_memory(None)
+    test_execution_log_without_file_system(None)
     # pytest.main([__file__])
