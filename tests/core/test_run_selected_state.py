e6d475a91354a4890836f391efe89a7e2cd8dba4
e.nakhle <elio-nakhle@agile-robots.com>
2021-11-23 10:50:36 +0100

    fix: Test run selected

--- tests/core/test_run_selected_state.py
+++ tests/core/test_run_selected_state.py
@@ -14,25 +14,6 @@ from tests.utils import wait_for_execution_engine_sync_counter
 logger = log.get_logger(__name__)
 
 
-def execute_command_synchronized_on_state(state_machine, state_id, command, counter=1, join=True):
-    old_run_id = state_machine.get_state_by_path(state_id).run_id
-    old_execution_counter = state_machine.get_state_by_path(state_id)._execution_counter
-    getattr(rafcon.core.singleton.state_machine_execution_engine, command)()
-    # let the state start properly
-    while old_run_id == state_machine.get_state_by_path(state_id).run_id:
-        time.sleep(0.005)
-    while old_execution_counter == state_machine.get_state_by_path(state_id)._execution_counter:
-        time.sleep(0.005)
-    if join:
-        try:
-            state_machine.get_state_by_path(state_id).join()
-        except RuntimeError:
-            # if the state is already executed then join() returns with an RuntimeError
-            pass
-    # let the hierarchy properly chose the next state
-    wait_for_execution_engine_sync_counter(counter, logger)
-
-
 def test_run_this_state(caplog):
     # run_selected
     # Initialize testing environment
@@ -41,16 +22,18 @@ def test_run_this_state(caplog):
     state_machine = storage.load_state_machine_from_path(testing_utils.get_test_sm_path(os.path.join(
         "unit_test_state_machines", "test_run_this_state")))
     rafcon.core.singleton.state_machine_manager.add_state_machine(state_machine)
-
-    # Run selected state machine (NDIVLD)
-    execute_command_synchronized_on_state(state_machine, "BTWFZQ/EPQSTG", "run_selected", 1)
-    # Stop execution engine
-    rafcon.core.singleton.state_machine_execution_engine.stop()
+    # Set state machine as active state machine
+    rafcon.core.singleton.state_machine_manager.active_state_machine_id = state_machine.state_machine_id
+    # Run selected state machine
+    rafcon.core.singleton.state_machine_execution_engine.run_selected_state("BTWFZQ/EPQSTG",
+                                                                            state_machine.state_machine_id)
     # assert variable state
     try:
         assert rafcon.core.singleton.global_variable_manager.get_variable("test_value") == 2
     # Shutdown testing environment
     finally:
+        # Stop execution engine
+        rafcon.core.singleton.state_machine_execution_engine.stop()
         testing_utils.shutdown_environment_only_core(caplog=caplog)
 
 
