75a163206306813d8869c17cf6591b32e7bf8f95
Vahid Mohammadi Gahrooei <vahid.mohammadi@agile-robots.com>
2021-11-25 15:57:27 +0100

    fix: refactor codes

--- source/rafcon/core/states/container_state.py
+++ source/rafcon/core/states/container_state.py
@@ -1643,15 +1643,16 @@ class ContainerState(State):
         for output_name, value in dictionary.items():
             for output_data_port_key, data_port in list(state.output_data_ports.items()):
                 if output_name == data_port.name:
-                    if value is None:
-                        logger.warning("The value of output port is 'None'. It has replaced with the default value.".
-                                       format(output_name, data_port.data_type, type(value)))
-                    elif not isinstance(value, data_port.data_type):
+                    if not isinstance(value, data_port.data_type):
                         if (not ((type(value) is float or type(value) is int) and
                                      (data_port.data_type is float or data_port.data_type is int)) and
                                 not (isinstance(value, type(None)))):
                             logger.error("The data type of output port {0} should be of type {1}, but is of type {2}".
                                          format(output_name, data_port.data_type, type(value)))
+                        elif value is None:
+                            logger.warning(
+                                "The value of output port is 'None'. It has replaced with the default value.".
+                                format(output_name, data_port.data_type, type(value)))
                     self.scoped_data[str(output_data_port_key) + state.state_id] = \
                         ScopedData(data_port.name, value, type(value), state.state_id, OutputDataPort, parent=self)
 
