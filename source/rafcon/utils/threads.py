b97276690e5b5200fb13a8007a5e0c224fd48e7c
Vahid Mohammadi Gahrooei <vahid.mohammadi@agile-robots.com>
2021-12-01 14:20:59 +0100

    fix: refactor codes

--- source/rafcon/utils/threads.py
+++ source/rafcon/utils/threads.py
@@ -24,7 +24,8 @@ class ReaderWriterLock:
 
     class Counter:
         readers = 0
-        writers = 0
+        pending_writers = 0
+        is_writing = False
 
     class ReaderLock:
         def __init__(self, condition, counter, resource):
@@ -34,7 +35,7 @@ class ReaderWriterLock:
 
         def __enter__(self):
             with self._condition:
-                while self._counter.writers > 0:
+                while self._counter.is_writing or self._counter.pending_writers > 0:
                     self._condition.wait()
                 self._counter.readers += 1
             return self._resource
@@ -53,13 +54,15 @@ class ReaderWriterLock:
 
         def __enter__(self):
             self._condition.acquire()
-            self._counter.writers += 1
-            while self._counter.readers > 0:
+            self._counter.pending_writers += 1
+            while self._counter.readers > 0 or self._counter.is_writing:
                 self._condition.wait()
+            self._counter.pending_writers -= 1
+            self._counter.is_writing = True
             return self._resource
 
         def __exit__(self, exc_type, exc_value, traceback):
-            self._counter.writers -= 1
+            self._counter.is_writing = False
             self._condition.notifyAll()
             self._condition.release()
 
