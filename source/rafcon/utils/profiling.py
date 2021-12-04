16de70c7f689aee66c26cb1b756b932844f15167
Vahid Mohammadi Gahrooei <vahid.mohammadi@agile-robots.com>
2021-12-04 14:28:43 +0100

    feat: add memory profiling for core too

---
+++ source/rafcon/utils/profiling.py
@@ -0,0 +1,21 @@
+import os
+import time
+import tracemalloc
+
+
+def memory_profiling(args):
+    memory_profiling_path = args['memory_profiling_path']
+    memory_profiling_interval = int(args['memory_profiling_interval'])
+    memory_profiling_print = args['memory_profiling_print']
+    while not args['stop']:
+        snapshot = tracemalloc.take_snapshot()
+        time.sleep(memory_profiling_interval)
+        with open(os.path.join(memory_profiling_path, 'memory_profiling.log'), 'a') as file:
+            file.write('[ Top 10 Differences ]\n')
+            if memory_profiling_print:
+                print('[ Top 10 Differences ]\n')
+            for result in tracemalloc.take_snapshot().compare_to(snapshot, 'lineno')[:10]:
+                file.write(str(result) + '\n')
+                if memory_profiling_print:
+                    print(result)
+            file.write('\n\n\n')
