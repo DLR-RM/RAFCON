3eb11935c8a4282913ce9b55e3c8a9b70f7e8112
Vahid Mohammadi Gahrooei <vahid.mohammadi@agile-robots.com>
2021-12-08 10:11:17 +0100

    feat: fix test

--- source/rafcon/utils/profiling.py
+++ source/rafcon/utils/profiling.py
@@ -10,9 +10,13 @@ def memory_profiling(args):
     memory_profiling_path = args['memory_profiling_path']
     memory_profiling_interval = int(args['memory_profiling_interval'])
     memory_profiling_print = args['memory_profiling_print']
+    interval_step = 0.1
     while not args['stop']:
         snapshot = tracemalloc.take_snapshot()
-        time.sleep(memory_profiling_interval)
+        interval = 0.0
+        while interval < memory_profiling_interval and not args['stop']:
+            time.sleep(interval_step)
+            interval += interval_step
         with open(os.path.join(memory_profiling_path, 'memory_profiling.log'), 'a') as file:
             file.write('[ Top 10 Differences ]\n')
             if memory_profiling_print:
