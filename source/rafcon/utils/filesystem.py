0e417b2af3f4165df666d2fa0e2c3a0fba71f85a
Vahid Mohammadi Gahrooei <vahid.mohammadi@agile-robots.com>
2021-12-08 16:09:13 +0100

    feat: add DEFAULT_LOG_PATH config

--- source/rafcon/utils/filesystem.py
+++ source/rafcon/utils/filesystem.py
@@ -19,8 +19,10 @@
 import os
 import tarfile
 import stat
-import shutil
-from os.path import realpath, dirname, join, expanduser
+
+from os.path import join, expanduser
+from rafcon.core.config import global_config
+
 import shutil, errno
 
 
@@ -114,10 +116,11 @@ def get_default_config_path():
 
 
 def get_default_log_path():
+    default_path = None
     home_path = expanduser('~')
     if home_path:
-        return join(home_path, ".log", "rafcon")
-    return None
+        default_path = join(home_path, ".log", "rafcon")
+    return global_config.get_config_value("DEFAULT_LOG_PATH", default_path)
 
 
 def separate_folder_path_and_file_name(path):
