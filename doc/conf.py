f22d5fe6d77c01146f100f6840b245190f013dae
erns_jn <j.ernst@dlr.de>
2024-03-22 15:19:53 +0100

    chore(docs): Rollback to alabaster layout

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,7 @@ setup_l10n()
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '5.0'
+needs_sphinx = '1.4'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -138,7 +138,7 @@ extlinks = {
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
